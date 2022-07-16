a37ade5656427cb98a05d50ac8320e413616c608
src48597962 <48597962@qq.com>
2022-07-16 12:21:07 +0000

    update SrcJySet.js.

--- SrcJySet.js
+++ SrcJySet.js
@@ -1275,6 +1275,9 @@ function jiekou(lx,data) {
                 let apigroup = getMyVar('apigroup');
                 if(lx=="update"){
                     isupdate = 1;
+                    log(data)
+                    log(urltype)
+                    log(apigroup)
                     if((apiurl==data.url&&apiname==data.name&&apiua==data.ua&&urltype==data.type&&apigroup==data.group)){
                         return "toast://未修改";
                     }
