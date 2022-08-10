08d839fc992a740be8c7ffd5f424648bda4e499f
jevons0411 <jevons0411@163.com>
2022-08-10 16:40:19 +0800

    Update SrcJuying.js

--- SrcJuying.js
+++ SrcJuying.js
@@ -182,8 +182,7 @@ function jiekouyiji() {
                     return a - b
                 })
             };
-            if(type_pids.indexOf(getMyVar('SrcJydouli$type_id',''))==-1){putMyVar('SrcJydouli$type_id',''+type_pids[0])}
-            
+            if(!getMyVar('SrcJydouli$type_id')||type_pids.indexOf(getMyVar('SrcJydouli$type_id',''))==-1){putMyVar('SrcJydouli$type_id',''+type_pids[0])}
             for (var j in type_pids) {
                 for (var i in typeclass) {
                     if(typeclass[i].type_pid==type_pids[j]){
