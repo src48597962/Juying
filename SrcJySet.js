c5f772ca555c51dab68cb693a7f6831bfdce5026
jevons0411 <jevons0411@163.com>
2022-08-11 13:38:13 +0800

    Update SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -2,12 +2,9 @@
 function SRCSet() {
 
 
-//let xmlhtml = request('http://f2dcj6.com/sapi?ac=videolist&pg=1&t=3');
-const X2JS = require('https://gitcode.net/src48597962/hktest/-/raw/master/we-x2js.js');
-let x2js = new X2JS();
-let xmlStr = '<xml><appId><![CDATA[wx123456]]></appId><nonceStr><![CDATA[123456789]]></nonceStr></xml>'
-let myJson = x2js.xml2js(xmlStr);
-log(myJson)
+let xmlhtml = request('http://f2dcj6.com/sapi?ac=videolist&pg=1&t=3');
+let video = phda(xmlhtml,'list&&video');
+log(video)
 
 
 
