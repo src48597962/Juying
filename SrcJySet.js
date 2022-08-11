62f8cf2285e339d7909b76d9659d30961191278b
jevons0411 <jevons0411@163.com>
2022-08-11 13:38:39 +0800

    Update SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -3,7 +3,7 @@ function SRCSet() {
 
 
 let xmlhtml = request('http://f2dcj6.com/sapi?ac=videolist&pg=1&t=3');
-let video = phda(xmlhtml,'list&&video');
+let video = pdha(xmlhtml,'list&&video');
 log(video)
 
 
