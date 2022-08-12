c5c08f7aef3b3bbde383c0080f8a6c5dbf992fc5
jevons0411 <jevons0411@163.com>
2022-08-12 13:43:57 +0800

    Update SrcJuying.js

--- SrcJuying.js
+++ SrcJuying.js
@@ -975,7 +975,7 @@ function sousuo() {
 
 //版本检测
 function Version() {
-    var nowVersion = '3.7';//现在版本
+    var nowVersion = "3.8";//现在版本
     var nowtime = Date.now();
     var oldtime = parseInt(getItem('VersionChecktime','0').replace('time',''));
     if (getMyVar('SrcJuying-VersionCheck', '0') == '0' && nowtime > (oldtime+6*60*60*1000)) {
