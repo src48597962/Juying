f413d36875575d221f4cbf7ced27192eaee13670
jevons0411 <jevons0411@163.com>
2022-09-07 16:28:19 +0800

    Update SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -576,8 +576,8 @@ function SRCSet() {
                         html = html.replace(reg, function(word) { 
                             return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word; 
                         }).replace(/^.*#.*$/gm,"").replace(/🐝|🚀|🐌|💡|🥇|⚽|🏀|📺|🐨|🐧|🍋|🐯|👒|🅱|🚁|🍎|🎈|💘|🐞|🔥|🌎|🈲|🍀|🥒|⭐️|❄️|\(XPF\)|\(萝卜\)|\(神马\)|\(切\)|\(聚\)|\(优\)|\(神马\)|\(XB\)|\(SP\)|[\t\r\n]/g,'').replace(/\,\,/g,',');
-                        log(html);
                         var data = JSON.parse(html);
+                        log('ok');
                         var jiekou = data.sites;
                         var jiexi = data.parses;
                     } catch (e) {
