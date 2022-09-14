5b7a058f6a195a2f5d7b4d228095b61fb2e5f70e
jevons0411 <jevons0411@163.com>
2022-09-14 11:20:26 +0800

    Update SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -1275,7 +1275,46 @@ function getSimilarity(str1,str2) {
     let length = str1.length > str2.length ? str1.length : str2.length
     return (sameNum/length) * 100 || 0
 }
-
+function similar(s, t, f) {//判断两个字符串之间的相似度
+    if (!s || !t) {
+        return 0
+    }
+    if(s === t){
+        return 100;
+    }
+    var l = s.length > t.length ? s.length : t.length
+    var n = s.length
+    var m = t.length
+    var d = []
+    f = f || 2
+    var min = function (a, b, c) {
+        return a < b ? (a < c ? a : c) : (b < c ? b : c)
+    }
+    var i, j, si, tj, cost
+    if (n === 0) return m
+    if (m === 0) return n
+    for (i = 0; i <= n; i++) {
+        d[i] = []
+        d[i][0] = i
+    }
+    for (j = 0; j <= m; j++) {
+        d[0][j] = j
+    }
+    for (i = 1; i <= n; i++) {
+        si = s.charAt(i - 1)
+        for (j = 1; j <= m; j++) {
+            tj = t.charAt(j - 1)
+            if (si === tj) {
+                cost = 0
+            } else {
+                cost = 1
+            }
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+        }
+    }
+    let res = (1 - d[n][m] / l) *100
+    return res.toFixed(f)
+}
 function jiekousave(urls,update) {
     try{
         var filepath = "hiker://files/rules/Src/Juying/jiekou.json";
@@ -1304,7 +1343,8 @@ function jiekousave(urls,update) {
             }
 
             function checkitem(item) {
-                //log(item.name+' '+urlname+' '+urltype+getSimilarity(item.name,urlname));
+                log(item.name+' '+urlname+' '+getSimilarity(item.name,urlname)+'dz>'+similar(item.name,urlname));
+                
                 return item.url==urlurl||(getSimilarity(item.name,urlname)>75&&urltype=="biubiu");
             }
             if(!datalist.some(checkitem)&&urlname&&/^http|^csp/.test(urlurl)&&urltype){
