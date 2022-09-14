2854db383f1fbaf844bc5d3790fe934e640cd62d
jevons0411 <jevons0411@163.com>
2022-09-14 11:09:57 +0800

    Update SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -1263,7 +1263,7 @@ function getSimilarity(str1,str2) {
     //寻找相同字符
     for (let i = 0; i < str1.length; i++) {
         for(let j =0;j<str2.length;j++){
-            if(str1[i]===str2[j]){
+            if(str1[i]==str2[j]){
                 sameNum ++ 
                 break
             }
