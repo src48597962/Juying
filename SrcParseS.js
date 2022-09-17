956f3e2d9e4a3881d9eb30860794d3ce49768dc7
jevons0411 <jevons0411@163.com>
2022-09-17 10:52:29 +0800

    Update SrcParseS.js

--- SrcParseS.js
+++ SrcParseS.js
@@ -787,7 +787,7 @@ var SrcParseS = {
                     iscalldn = 1;//断插辅助只调用一轮
                     UrlList.push({type:'dn'});
                 }
-                log(UrlList)
+
                 let Urlparses = UrlList.map((list)=>{
                     if(list.type=="dn"){
                         return {func: dnaytmParse, param: vipUrl, id: 'dn'}
@@ -804,7 +804,7 @@ var SrcParseS = {
                         }
                     }
                 });
-                log($.stringify(Urlparses))
+
                 be(Urlparses, {
                     func: function(obj, id, error, taskResult) {
                         let beurl = taskResult.url;
