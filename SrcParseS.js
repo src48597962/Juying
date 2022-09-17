c49337a35adf6dda18f15523aba72f2e8c964bbe
jevons0411 <jevons0411@163.com>
2022-09-17 18:34:17 +0800

    Update SrcParseS.js

--- SrcParseS.js
+++ SrcParseS.js
@@ -45,7 +45,7 @@ var SrcParseS = {
         } else if (/wkfile/.test(url)) {
             var header = { 'User-Agent': 'Mozilla/5.0', 'Referer': 'fantuan.tv' };
         } else {
-            var header = { 'User-Agent': 'Mozilla/5.0' };
+            var header = {};
         }
         return header;
     },
