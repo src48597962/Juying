faa20addbc7eae9057e297977c6b000e6743858b
src48597962 <48597962@qq.com>
2022-09-25 13:21:06 +0000

    更新 'SrcJyMenu.js'

--- SrcJyMenu.js
+++ SrcJyMenu.js
@@ -402,7 +402,7 @@ var erjimenu = [
                     });
                     d.push({
                         title:  (JYconfig.superweb==1?getide(1):getide(0)) + '播放器超级嗅探：' + (JYconfig.superweb==1?'开启':'关闭'),
-                        desc: JYconfig.superweb==1?'此模式目前无法在选集页下载，无法预加载，但是可以多线路':'普通模式先嗅探到播放地址再进播放器',
+                        desc: JYconfig.superweb==1?'无法在选集页下载，无法预加载，但是可以多线路':'普通模式先嗅探到播放地址再进播放器',
                         url: $('#noLoading#').lazyRule((JYconfig,cfgfile) => {
                             if(JYconfig['superweb'] != 1){
                                 JYconfig['superweb'] = 1;
