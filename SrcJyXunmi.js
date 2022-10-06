996f78d2c710d3e0a86b50d126bca529f2df7c8a
src48597962 <48597962@qq.com>
2022-10-06 18:30:21 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -23,6 +23,7 @@ function xunmi(name,data,ishkss) {
             putMyVar('xunminum',JYconfig['xunminum']?JYconfig['xunminum']:"10");
             putMyVar('xunmitimeout',JYconfig['xunmitimeout']?JYconfig['xunmitimeout']:"5");
             putMyVar('failnum',JYconfig['failnum']?JYconfig['failnum']:"10");
+            if(JYconfig.cachem3u8==1&&JYconfig.superweb==1){putMyVar('superwebcachem3u8','1')}else{clearMyVar('superwebcachem3u8')}
         }
         var xunmigroup = JYconfig.xunmigroup&&JYconfig.xunmigroup!="全部"?JYconfig.xunmigroup:"";
     }catch(e){}
