d32941233a1be9e27c7b2ef1e64e2000422f39fe
src48597962 <48597962@qq.com>
2022-10-13 10:22:46 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -863,19 +863,19 @@ function xunmierji(type,ua) {
             var arts = html.videolist;
             var conts = arts;
         }else if (/xpath/.test(type)) {
-            log('进来了')
             try{
                 getsm = "获取传递数据";
                 var jsondata = MY_PARAMS.data;
                 getsm = "获取播放选集列表";
                 var arts = xpathArray(html, jsondata.dtNode+jsondata.dtFromNode+jsondata.dtFromName);
-                log(arts)
                 var conts = [];
                 for (let i = 1; i < arts.length+1; i++) {
                     if(arts[i-1].indexOf("在线视频")>-1){arts[i-1] = '播放源'+i;}
-                    let contname = xpathArray(html, '('+jsondata.dtNode+jsondata.dtUrlNode+')['+i+']'+jsondata.dtUrlSubNode+jsondata.dtUrlName);
-                    let conturl = xpathArray(html, '('+jsondata.dtNode+jsondata.dtUrlNode+')['+i+']'+jsondata.dtUrlSubNode+jsondata.dtUrlId);
+                    log(i)
+                    //let contname = xpathArray(html, '('+jsondata.dtNode+jsondata.dtUrlNode+')['+i+']'+jsondata.dtUrlSubNode+jsondata.dtUrlName);
+                    let contname = xpathArray(html, jsondata.dtUrlNode+'['+i+']'+jsondata.dtUrlSubNode+jsondata.dtUrlName);
                     log(contname)
+                    let conturl = xpathArray(html, '('+jsondata.dtNode+jsondata.dtUrlNode+')['+i+']'+jsondata.dtUrlSubNode+jsondata.dtUrlId);
                     log(conturl)
                     let cont = [];
                     for (let j = 0; j < contname.length; j++) {
