1cd7d3c64d93cc1f1f80109564e1374c823b2014
src48597962 <48597962@qq.com>
2022-10-13 21:46:10 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -424,7 +424,7 @@ function xunmi(name,data,ishkss) {
                             for(var j in title){
                                 list.push({"id":href[j],"name":title[j],"pic":img[j],"desc":mark[j]})
                             }
-                            var ssvodurl = `jsondata.dtUrl.replace(/.*\/|\.html/g,'')+list.id`;
+                            var ssvodurl = `jsondata.dtUrl.replace('{vid}',list.id.replace('.html',''))`;
                         }
                     }else{
                         var ssurl = jsondata.url+jsondata.sousuoqian+name+jsondata.sousuohou;
