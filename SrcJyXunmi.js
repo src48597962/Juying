b7c1a7d0668f0236bdcac6c1cce11be72a79bb33
src48597962 <48597962@qq.com>
2022-10-15 09:19:25 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -46,22 +46,30 @@ function xunmi(name,data,ishkss) {
                 }else{
                     var DYdatalist = [];
                     let TVBoxDY = JYconfig.TVBoxDY;
-                    if(/\/storage\/emulated\//.test(TVBoxDY)){TVBoxDY = "file://" + TVBoxDY}
-                    if(/^http/.test(TVBoxDY)){
-                        //var dyhtml = fetchCache(TVBoxDY,96, { timeout:2000 });
+                    try{
+                        showLoading('正在多线程加载TVBox订阅接口');
+                        if(/\/storage\/emulated\//.test(TVBoxDY)){TVBoxDY = "file://" + TVBoxDY}
+                        /*
+                        if(/^http/.test(TVBoxDY)){
+                            var dyhtml = fetchCache(TVBoxDY,96, { timeout:2000 });
+                        }else{
+                            var dyhtml = fetch(TVBoxDY);
+                        }*/
                         var dyhtml = request(TVBoxDY,{ timeout:2000 });
-                    }else{
-                        var dyhtml = fetch(TVBoxDY);
+                        var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n|$))|(\/\*(\n|.)*?\*\/)/g;
+                        dyhtml = dyhtml.replace(reg, function(word) { 
+                            return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word; 
+                        }).replace(/^.*#.*$/gm,"").replace(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])|\(XP\)|\(XPF\)|\(萝卜\)|\(切\)|\(聚\)|\(优\)|\(神马\)|\(XB\)|\(SP\)|[\x00-\x1F\x7F]/g,'').replace(/\,\,/g,',');
+                        //var dydata = JSON.parse(dyhtml);
+                        eval('var dydata = ' + dyhtml)
+                        var dyjiekou = dydata.sites||[];
+                        require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJySet.js');
+                    } catch (e) {
+                        hideLoading();
+                        log('TVBox订阅文件检测失败>'+e.message); 
+                        var dyjiekou = [];
                     }
-                    var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n|$))|(\/\*(\n|.)*?\*\/)/g;
-                    dyhtml = dyhtml.replace(reg, function(word) { 
-                        return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word; 
-                    }).replace(/^.*#.*$/gm,"").replace(/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])|\(XPF\)|\(萝卜\)|\(切\)|\(聚\)|\(优\)|\(神马\)|\(XB\)|\(SP\)|[\x00-\x1F\x7F]/g,'').replace(/\,\,/g,',');
-                    //var dydata = JSON.parse(dyhtml);
-                    eval('var dydata = ' + dyhtml)
-                    var dyjiekou = dydata.sites;
-                    require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJySet.js');
-                    showLoading('正在多线程加载TVBox订阅接口');
+
                     //多线程处理
                     var dytask = function(obj) {
                         if(/^csp_AppYs/.test(obj.api)){                        
@@ -81,12 +89,13 @@ function xunmi(name,data,ishkss) {
                                 if(/^clan:/.test(urlfile)){
                                     urlfile = urlfile.replace("clan://TVBox/",TVBoxDY.match(/file.*\//)[0]);
                                 }
+                                /*
                                 if(/^http/.test(urlfile)){
-                                    //var biuhtml = fetchCache(urlfile,168,{timeout:2000});
-                                    var biuhtml = request(urlfile, {timeout:2000});
+                                    var biuhtml = fetchCache(urlfile,168,{timeout:2000});
                                 }else{
                                     var biuhtml = fetch(urlfile);
-                                }
+                                }*/
+                                var biuhtml = request(urlfile, {timeout:2000});
                                 biuhtml = biuhtml.replace(reg, function(word) { 
                                     return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word; 
                                 }).replace(/^.*#.*$/mg,"").replace(/[\t\r\n]/g,'');
