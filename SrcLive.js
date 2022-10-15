79833513a9df81b22a9c12a14a9baa70f9dd44f0
src48597962 <48597962@qq.com>
2022-10-15 22:02:49 +0800

    更新 'SrcLive.js'

--- SrcLive.js
+++ SrcLive.js
@@ -18,8 +18,8 @@ function Live() {
         for(let i=0;i<JYlives.length;i++){
             if(JYlives[i].indexOf('#genre#')>-1){
                 group = JYlives[i].split(',')[0];
-            }else if(JYlives[i].indexOf(',')>-1&&!datalist.some(item => item.name==JYlives[i].split(',')[0])){
-                datalist.push({group: group, name: JYlives[i].split(',')[0]});
+            }else if(JYlives[i].indexOf(',')>-1){
+                datalist.push({group: group, name: JYlives[i].split(',')[0], url: JYlives[i].split(',')[1]});
             }
         }
         log(1)
