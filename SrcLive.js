326722cfd99f56a2929ad7cc104920f57156f704
src48597962 <48597962@qq.com>
2022-10-15 21:11:33 +0800

    更新 'SrcLive.js'

--- SrcLive.js
+++ SrcLive.js
@@ -12,6 +12,17 @@ function SRCSet() {
         var datalist = [];
     }
     if(datalist.length>0){
+
+        //writeFile(livefile, "");
+        for (let i=0;i<datalist.length;i++) {
+            if(datalist[i].indexOf('#genre#')==-1){
+                d.push({
+                    title: datalist[i].split(',')[0],
+                    col_type:'text_4',
+                    url: datalist[i].split(',')[1]
+                });
+            }
+        }
         d.push({
             title: '<br>',
             col_type: 'rich_text'
