0bce7694ff75fc935880861bb73e086f556dd48d
src48597962 <48597962@qq.com>
2022-10-18 15:38:33 +0800

    更新 'SrcLive.js'

--- SrcLive.js
+++ SrcLive.js
@@ -139,8 +139,8 @@ function Live() {
                                 writeFile(JYlivefile, JYlives.join('\n'));
                                 hideLoading();
                                 refreshPage(false);
-                                require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcLive.js');
-                                editmode();
+                                //require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcLive.js');
+                                //editmode();
                                 return "toast://已删除分组 <"+groupname+"> 所有地址";
                             }catch(e){
                                 hideLoading();
@@ -198,10 +198,58 @@ function Live() {
         });
     }
     setHomeResult(d);
-    editmode();
+    if(getMyVar('editmode','0')!="0"){
+        let editnames = ["分组删除|groupdelete","分组改名|grouprename","地址删除|urldelete","地址改名|urlrename","退出编辑|exitedit"];
+        let editmenu = [];
+        for(let i=0;i<editnames.length;i++){
+            let name = editnames[i].split('|')[0];
+            let code = editnames[i].split('|')[1];
+            editmenu.push({
+                title: name,
+                url: $("#noLoading#").lazyRule((name,code,editnames) => {
+                    if(code=="exitedit"){
+                        clearMyVar('editmode');
+                        deleteItemByCls('editmenu');
+                        return "toast://退出编辑，正常观看";
+                    }else{
+                        putMyVar('editmode',code);
+                    }
+                    for(let i in editnames){
+                        if(editnames[i].split('|')[1]==code){
+                            updateItem(code,{title:'‘‘’’<b><span style="color:#3399cc">'+name})
+                        }else{
+                            updateItem(editnames[i].split('|')[1],{title:editnames[i].split('|')[0]})
+                        }
+                    }
+                    return "toast://进入"+name+"模式";
+                },name,code,editnames),
+                col_type: 'scroll_button',
+                extra: {
+                    id: code,
+                    cls: 'editmenu'
+                }
+            })
+        }
+        editmenu.push({
+            col_type: 'line',
+            extra: {
+                cls: 'editmenu'
+            }
+        })
+        for (let i = 0; i < 9; i++) {
+            editmenu.push({
+                col_type: "blank_block",
+                extra: {
+                    cls: 'editmenu'
+                }
+            })
+        }
+        addItemAfter('livesearch',editmenu);
+    }
+    //editmode();
 }
 function editmode(){
-    if(getMyVar('editmode','0')==1){
+    if(getMyVar('editmode','0')!="0"){
         let editnames = ["分组删除|groupdelete","分组改名|grouprename","地址删除|urldelete","地址改名|urlrename","退出编辑|exitedit"];
         let editmenu = [];
         for(let i=0;i<editnames.length;i++){
