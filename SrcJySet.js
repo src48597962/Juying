30391019853f286ec3b77aa1013d2fbae85142c6
src48597962 <48597962@qq.com>
2022-10-19 18:36:19 +0800

    更新 'SrcJySet.js'

--- SrcJySet.js
+++ SrcJySet.js
@@ -2252,7 +2252,7 @@ function Resourceimport(input,importtype,boxdy){
             var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n|$))|(\/\*(\n|.)*?\*\/)/g;
             html = html.replace(/api\"\:csp/g,'api":"csp').replace(reg, function(word) { 
                 return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word; 
-            }).replace(/^.*#.*$/gm,"").replace(/\,\,/g,',').replace(/\s+/g, "").replace(/<\/?.+?>/g,"").replace(/[\r\n]/g, "").replace(/\"\}.*?\{\"key\"/g,'"},{"key"');//.replace(/=\\n\"/g,'="')|[\t\r\n]
+            }).replace(/^.*#.*$/gm,"").replace(/\,\,/g,',');//.replace(/=\\n\"/g,'="')|[\t\r\n].replace(/\s+/g, "").replace(/<\/?.+?>/g,"").replace(/[\r\n]/g, "")
             //log(html);
             eval('var data = ' + html)
             //var data = JSON.parse(html);                        
