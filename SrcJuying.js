5822e7ca35c2a10a1135390eb3e3c42d855a73c1
src48597962 <48597962@qq.com>
2022-10-22 23:18:23 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -993,7 +993,7 @@ function sousuo() {
 
 //版本检测
 function Version() {
-    var nowVersion = "5.3";//现在版本 
+    var nowVersion = "5.4";//现在版本 
     var nowtime = Date.now();
     var oldtime = parseInt(getItem('VersionChecktime','0').replace('time',''));
     if (getMyVar('SrcJuying-VersionCheck', '0') == '0' && nowtime > (oldtime+24*60*60*1000)) {
