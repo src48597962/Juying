daa8591fc31abd0f0d6827430de66ce338b82123
src48597962 <48597962@qq.com>
2022-10-22 12:47:08 +0800

    更新 'SrcParseS.js'

--- SrcParseS.js
+++ SrcParseS.js
@@ -52,7 +52,7 @@ var SrcParseS = {
     嗅探: function (vipUrl, excludeurl) {
         showLoading('√视频解析中，请稍候...');
         excludeurl = excludeurl||[];
-        return (getMyVar('SrcXTNH', 'web') == 'x5' ? 'x5Rule://' : 'webRule://') + vipUrl + '@' + $.toString((formatUrl,vipUrl,excludeurl) => {
+        return (getMyVar('SrcXTNH', 'web') == 'x5' ? 'x5Rule://' : 'webRule://') + vipUrl + '@' + $.toString((formatUrl,vipUrl,excludeurl,testurl) => {
             if (window.c == null) {
                 if (typeof (request) == 'undefined' || !request) {
                     eval(fba.getInternalJs());
@@ -76,10 +76,10 @@ var SrcParseS = {
             var exclude = /\/404\.m3u8|\/xiajia\.mp4|\/余额不足\.m3u8|\.css|\.js|\.gif|\.png|\.jpeg|api\.m3u88\.com|html,http|\.php\?v=h|\?url=h|\&url=h|%253Furl%253Dh/;//设置排除地址
             var contain = /\.mp4|\.m3u8|\.flv|\.avi|\.mpeg|\.wmv|\.mov|\.rmvb|\.dat|qqBFdownload|mime=video%2F|video_mp4|\.ts\?|TG@UosVod/;//设置符合条件的正确地址
             for (var i in urls) {
-                if (!exclude.test(urls[i]) && contain.test(urls[i]) && excludeurl.indexOf(urls[i])==-1) {
+                if (!exclude.test(urls[i]) && contain.test(urls[i]) && excludeurl.indexOf(urls[i])==-1 && testurl(urls[i])==1) {
                     //fba.log("嗅探成功>"+urls[i]);
                     //return urls[i]+'#isVideo=true#';
-                    //if(fy_bridge_app.getHeaderUrl&&vipUrl.indexOf("=http")==-1)
+                    if(fy_bridge_app.getHeaderUrl&&vipUrl.indexOf("=http")==-1)
                         return $$$("#noLoading#").lazyRule((url) => {
                             url = base64Decode(url);
                             if (getMyVar('SrcM3U8', '1') == "1"&&url.indexOf('.m3u8')>-1) {
@@ -88,15 +88,15 @@ var SrcParseS = {
                                 return url.replace(";{", "#ignoreImg=true##isVideo=true#;{");
                             }
                         }, fy_bridge_app.base64Encode(fy_bridge_app.getHeaderUrl(urls[i])));
-                    /*else {
+                    else {
                         return $$$("#noLoading#").lazyRule((url, formatUrl) => {
                             //url = url.replace(/http.*?\?url=/, '');
                             return formatUrl(url)+"#ignoreImg=true#";
                         }, urls[i], formatUrl);
-                    }*/
+                    }
                 }
             }
-        }, this.formatUrl, vipUrl, excludeurl)
+        }, this.formatUrl, vipUrl, excludeurl, testvideourl)
     },
     智能: function (vipUrl, input) {
         showLoading('√智能解析中，请稍候');
@@ -1110,7 +1110,7 @@ var SrcParseS = {
             if(/vkey=/.test(url)){
                 return 1;
             }else if (/\.m3u8/.test(url)) {
-                var urlcode = JSON.parse(request(url,{withStatusCode:true,timeout:2000}));
+                var urlcode = JSON.parse(request(url,{withStatusCode:true,timeout:1500}));
                 //log(name+'url访问状态码：'+urlcode.statusCode)
                 if(urlcode.statusCode==-1){
                     log(name+'>m3u8探测超时未拦载，结果未知')
@@ -1135,7 +1135,7 @@ var SrcParseS = {
                             let http = urlcode.url.match(/http.*\//)[0];
                             urlts = http + urlts;
                         }    
-                        var tscode = JSON.parse(request(urlts,{headers:{'Referer':url},onlyHeaders:true,timeout:2000}));
+                        var tscode = JSON.parse(request(urlts,{headers:{'Referer':url},onlyHeaders:true,timeout:1500}));
                         //log(name+'ts访问状态码：'+tscode.statusCode)
                         if(tscode.statusCode==-1){
                             log(name+'>ts段探测超时未拦载，结果未知')
@@ -1147,7 +1147,7 @@ var SrcParseS = {
                     }
                 }
             }else if (/\.mp4/.test(url)) {
-                var urlheader = JSON.parse(request(url,{onlyHeaders:true,timeout:2000}));
+                var urlheader = JSON.parse(request(url,{onlyHeaders:true,timeout:1500}));
                 if(urlheader.statusCode==-1){
                     log(name+'>mp4探测超时未拦载，结果未知')
                     return 1;
