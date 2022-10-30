0f5144f3e4eaa70c75419929ff92d7cd9c4d1e3e
src48597962 <48597962@qq.com>
2022-10-30 09:51:16 +0800

    更新 'SrcJySet.js'

--- SrcJySet.js
+++ SrcJySet.js
@@ -2432,9 +2432,10 @@ function Resourceimport(input,importtype,boxdy){
                     if(/^http/.test(jiexi[i].url)){
                         let arr  = { "name": jiexi[i].name, "parse": jiexi[i].url, "stopfrom": [], "priorfrom": [], "sort": 1 };
                         if(jiexi[i].header){
-                            arr['header'] = JSON.parse(jiexi[i].header);
+                            arr['header'] = jiexi[i].header;
                         }
                         urls.push(arr);
+                        log(arr)
                     }
                 }
                 if(isboxdy){
