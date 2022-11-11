f6521f74177de277c507c7d0f1832c6cca8e151e
src48597962 <48597962@qq.com>
2022-11-11 23:42:46 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -71,7 +71,7 @@ function JYerji(){
     var Marksum = 30;//设置记录线路足迹数量
     var urlline = getMyVar(MY_URL, typeof(SrcMarkline) != "undefined"?SrcMarkline:'0');
     var d = [];
-    var html = request(MY_URL, { headers: { 'User-Agent': PC_UA } });
+    var html = request(datasource=='sougou'?MY_URL:MY_URL+'&site='+MY_PARAMS.sites[urlline], { headers: { 'User-Agent': PC_UA } });
     let json = datasource=="sougou"?JSON.parse(html.match(/INITIAL_STATE.*?({.*});/)[1]).detail.itemData:JSON.parse(html).data;
     let plays = datasource=="sougou"?json.play.item_list:[];
     let shows = datasource=="sougou"?json.play_from_open_index:'';
@@ -119,7 +119,9 @@ function JYerji(){
             if(parseInt(urlline)==i){
                 let sitename = tabs[i];
                 let onenum = playnum.length>0?playnum[sitename]||'0':'0';
-                json = JSON.parse(request(MY_URL+'&start=1&end='+onenum+'&site='+sitename, { headers: { 'User-Agent': PC_UA } })).data;
+                if(parseInt(onenum)>20){
+                    json = JSON.parse(request(MY_URL+'&start=1&end='+onenum, { headers: { 'User-Agent': PC_UA } })).data;
+                }
                 let onelist = json.allepidetail[sitename];
                 onelist = onelist.map(item=>{
                     return item.playlink_num+'$'+item.url;
@@ -577,7 +579,8 @@ function JYyiji(){
                 desc: list[i].total?list[i].total==list[i].upinfo?"全集"+list[i].total:"连载"+list[i].upinfo+"/"+list[i].total:list[i].tag?list[i].tag:list[i].doubanscore?list[i].doubanscore:"",
                 extra: {
                     pic: img,
-                    name: list[i].title
+                    name: list[i].title,
+                    sites: list[i].playlink_sites
                 }
             });
         }
