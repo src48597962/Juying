c74bed3faef94611825e98846fb2bc2582e7fd9a
src48597962 <48597962@qq.com>
2022-11-11 17:23:11 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -208,7 +208,8 @@ function homepage(datasource){
                             col_type: 'scroll_button'
                         });
                     }
-                    if(acts!=undefined&&filter[i].label=='明星'){
+                    log($.type(acts))
+                    if($.type(acts)!='undefined'&&filter[i].label=='明星'){
                         let act = acts[getMyVar('SrcJuying$地区', '全部')];
                         act.forEach(item => {
                             if($.type(item)!='string'){
