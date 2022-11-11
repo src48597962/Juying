dad22650e900e0f3abc5a7f648ac258efb2cea24
src48597962 <48597962@qq.com>
2022-11-11 16:48:30 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -194,7 +194,8 @@ function homepage(datasource){
                         d.push({
                             title: getMyVar('SrcJuying$'+filter[i].label, '全部')==optionname?'““””<span style="color:red">'+optionname+'</span>':optionname,
                             url: $('#noLoading#').lazyRule((name,option) => {
-                                    putMyVar('SrcJuying$'+name, option);
+                                    
+                                    putMyVar('SrcJuying$'+name, option?option:'全部');
                                     refreshPage(false);
                                     return "hiker://empty";
                                 }, filter[i].label, option_list[j].id),
