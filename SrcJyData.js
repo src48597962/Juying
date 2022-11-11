370402a294ac1c6c4b058d5f40d2cba5146648d9
src48597962 <48597962@qq.com>
2022-11-11 22:50:00 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -109,6 +109,7 @@ function JYerji(){
                 let onenum = playnum[sitename]||'0';
                 if(getMyVar(MY_URL, '0')=='0'&&parseInt(onenum)>20){
                     json = JSON.parse(request(myurl+'&start=1&end='+onenum, { headers: { 'User-Agent': PC_UA } }));
+                    log(json);
                 }
                 let onelist = json.allepidetail[sitename];
                 onelist = onelist.map(item=>{
