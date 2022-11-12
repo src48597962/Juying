0a8f4d3aede13f4489a867b403729ba199edac32
src48597962 <48597962@qq.com>
2022-11-12 13:36:42 +0800

    更新 'SrcJyMenu.js'

--- SrcJyMenu.js
+++ SrcJyMenu.js
@@ -418,7 +418,7 @@ var erjimenu = [
                     d.push({
                         title: getItem('hikertvboxset')?'参照频道香佬教程，需自行研究':'推送选集列表，设置接收端ip地址',
                         desc: getItem('hikertvboxset')?'接收端ip地址：'+getItem('hikertvboxset',''):'还未设置接收端ip地址',
-                        url: "input://" + (getItem('hikertvboxset','')==''?('http://' + boxip + ':9978'):getItem('hikertvboxset') + "////TVBOX接收端ip地址.js:setItem('hikertvboxset',input);refreshPage()",
+                        url: "input://" + (getItem('hikertvboxset','')==''?('http://' + boxip + ':9978'):getItem('hikertvboxset')) + "////TVBOX接收端ip地址.js:setItem('hikertvboxset',input);refreshPage()",
                         col_type: "text_center_1"
                     }); 
                 }
