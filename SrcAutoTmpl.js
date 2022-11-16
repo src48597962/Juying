b8817528f7500324f741c53c7890a3dd845e73f8
src48597962 <48597962@qq.com>
2022-11-16 21:21:14 +0800

    更新 'SrcAutoTmpl.js'

--- SrcAutoTmpl.js
+++ SrcAutoTmpl.js
@@ -1,8 +1,272 @@
-//直接白嫖道长dr模板
-let erjitmplfile = 'hiker://files/rules/dzHouse/json/二级模板.json';
-if(!fileExist(erjitmplfile)){
-    requireDownload('http://hiker.nokia.press/hikerule/rulelist.json?id=4552', erjitmplfile);
-}
+//白嫖道长dr二级模板数据
+let erjiTmpl = [
+	{
+        "id": 1,
+		"img": ".lazyload&&data-original",
+		"desc": ".data&&Text;.data,6&&Text",
+		"content": "p.detail&&Text.js:input.replace(\"简介：\",\"\").replace(\"详情\",\"\")",
+		"tabs": ".nav-tabs&&li",
+		"lists": ".stui-content__playlist;body&&li"
+	},
+	{
+        "id": 2,
+		"img": ".lazyload&&data-original",
+		"desc": "span.sketch.content&&Text",
+		"tabs": ".nav-tabs&&li",
+		"lists": ".myui-content__list;body&&li"
+	},
+	{
+        "id": 3,
+		"img": ".lazyload&&data-original",
+		"desc": ".module-info-item,1&&Text;.module-info-item,2&&Text;.module-info-item,3&&Text",
+		"content": ".module-info-introduction-content&&Text",
+		"tabs": "body&&.module-tab-item",
+		"lists": "body&&.module-play-list;body&&a"
+	},
+	{
+        "id": 4,
+		"img": ".myui-content__thumb&&a&&img&&data-original",
+		"desc": ".data,2&&Text;.data,3&&Text",
+		"content": "#desc&&span.data&&Text",
+		"tabs": "ul.nav-tabs&&li",
+		"lists": ".tab-content&&#id&&li"
+	},
+	{
+        "id": 5,
+		"img": ".lazyload&&data-src",
+		"desc": ".tag-link&&Text",
+		"content": ".video-info-content&&Text",
+		"tabs": "body&&.module-tab-item",
+		"lists": "body&&.scroll-box-y;body&&a"
+	},
+	{
+        "id": 6,
+		"img": ".lazyload&&data-original",
+		"desc": "p.data,-1&&Text;p.data,-2&&Text",
+		"content": "body&&.stui-pannel_bd:not(:has(a)):has(.col-pd)&&Text",
+		"tabs": "body&&.bottom-line:has(span)",
+		"lists": "body&&.stui-content__playlist;body&&li"
+	},
+	{
+        "id": 7,
+		"img": ".lazyload&&data-original",
+		"desc": "p.data&&Text;.stui-content__detail&&p,-2&&Text",
+		"content": ".detail&&Text",
+		"tabs": "body&&.stui-vodlist__head:has(span)",
+		"tab_text": "h3&&Text",
+		"lists": ".stui-content__playlist;body&&li"
+	},
+	{
+        "id": 8,
+		"img": ".lazyload&&data-original",
+		"desc": "p.data&&Text;.stui-content__detail&&p,-2&&Text",
+		"content": ".desc&&Text",
+		"tabs": "body&&.stui-pannel:has(span.more)",
+		"tab_text": ".title&&Text",
+		"lists": ".stui-content__playlist;body&&li"
+	},
+	{
+        "id": 9,
+		"img": ".fed-lazy&&data-original",
+		"desc": ".fed-list-remarks&&Text;.fed-deta-info&&li,-2&&Text;.fed-deta-info&&li,-3&&Text",
+		"content": ".fed-tabs-info&&p&&Text",
+		"tabs": "body&&.fed-play-btns",
+		"lists": ".fed-play-item;body&&ul,1&&li"
+	},
+	{
+        "id": 10,
+		"img": ".hl-lazy&&data-original",
+		"desc": ".hl-full-box&&li,1&&Text",
+		"content": ".hl-full-box&&li,-1&&Text",
+		"tabs": ".hl-from-list&&li",
+		"lists": "body&&.hl-plays-list;body&&li"
+	},
+	{
+        "id": 11,
+		"img": ".lazy&&data-original",
+		"desc": ".vodTag&&Text",
+		"content": ".ecshow&&Text",
+		"tabs": ".play_source_tab&&a",
+		"tab_text": "body&&Text",
+		"lists": "body&&.content_playlist;body&&a"
+	},
+	{
+        "id": 12,
+		"img": ".lazyload&&data-original",
+		"desc": ".content_min&&li,1&&Text",
+		"content": ".content_min&&li,-1&&Text",
+		"tabs": ".play_source_tab&&a",
+		"lists": "body&&.content_playlist;body&&li"
+	},
+	{
+        "id": 13,
+		"img": ".lazy&&data-original",
+		"desc": ".mv-showr&&p,2&&Text;.mv-showr&&p,3&&Text",
+		"content": ".des&&Text",
+		"tabs": ".layui-tab-brief,1&&.layui-tab-title&&li",
+		"lists": ".layui-tab-content,1&&.layui-tab-item:has(a);body&&a"
+	},
+	{
+        "id": 14,
+		"img": ".y-part-2by3&&data-original",
+		"desc": ".y-part-rows&&li&&Text&&Text;.y-part-rows&&li,1&&Text",
+		"tabs": ".nav-tabs&&li",
+		"lists": ".episodes-list;body&&li"
+	},
+	{
+        "id": 15,
+		"img": ".fed-part-2by3&&data-original",
+		"desc": ".fed-deta-content&&.fed-part-rows&&li&&Text",
+		"content": ".fed-conv-text&&Text",
+		"tabs": ".fed-tabs-foot&&li",
+		"lists": ".fed-tabs-btm;body&&li"
+	},
+	{
+        "id": 16,
+		"img": ".detail-pic&&img&&src",
+		"desc": ".addtime&&Text",
+		"content": ".info&&Text",
+		"tabs": ".tab_box&&h2",
+		"lists": "body&&.video_list;body&&a"
+	},
+	{
+        "id": 17,
+		"img": ".cover&&img&&src",
+		"desc": ".v_desc&&Text;#intro&&p,0&&Text;#intro&&p,2&&Text;#intro&&p,1&&Text;#intro&&p,3&&Text",
+		"content": "#intro&&p,-1&&Text;",
+		"tabs": ".tab_control&&li",
+		"lists": "#play_list&&.play_list;body&&li"
+	},
+	{
+        "id": 18,
+		"img": ".poster&&img&&src",
+		"desc": ".detail_imform_kv,0&&Text;.detail_imform_kv,2&&Text;.detail_imform_kv,5&&Text",
+		"content": ".detail_imform_desc_pre&&p&&Text",
+		"tabs": "#menu0&&li",
+		"lists": "#main0&&.movurl;body&&li"
+	},
+	{
+        "id": 19,
+		"img": ".lazy&&data-original",
+		"desc": ".info-txt&&p,2&&Text;.info-txt&&p,3&&Text;",
+		"content": ".brief&&Text",
+		"tabs": "body&&.playname",
+		"lists": ".playerlist;body&&ul&&a"
+	},
+	{
+        "id": 20,
+		"img": ".poster&&a&&style",
+		"desc": ".play-tag&&Text",
+		"content": ".info-wrap,-1&&Text",
+		"tabs": ".swiper-slide",
+		"lists": "#playsx;body&&li"
+	},
+	{
+        "id": 21,
+		"img": "",
+		"desc": "body&&.data&&Text",
+		"content": ".stui-player__detail&&p,-1&&Text",
+		"tabs": ".tab-top",
+		"lists": ".stui-play__list;body&&li"
+	},
+	{
+        "id": 22,
+		"img": ".pic&&img&&src",
+		"desc": "body&&dd,0&&Text;info&&dd,1&&Text;.info&&dd,2&&Text",
+		"content": ".desdd&&Text",
+		"tabs": ".tab0&&li:not(:matches(^$))",
+		"lists": "body&&.plist;body&&a"
+	},
+	{
+        "id": 23,
+		"img": ".pic&&img&&src",
+		"desc": "body&&dd,0&&Text;info&&dd,1&&Text;.info&&dd,2&&Text",
+		"content": ".desdd&&Text",
+		"tabs": "#tab11",
+		"lists": "body&&.plist;body&&a"
+	},
+	{
+        "id": 24,
+		"img": ".dyimg&&img&&src",
+		"desc": ".data,0&&Text",
+		"content": "#desc&&.stui-content__desc&&Text",
+		"tabs": "#bofy&&.t-ul&&li",
+		"lists": ".stui-content__playlist;body&&li"
+	},
+	{
+        "id": 25,
+		"img": ".lazyload&&data-original",
+		"desc": ".fed-deta-content&&.fed-part-rows&&Text",
+		"content": ".fed-tabs-info&&p&&Text",
+		"tabs": ".fed-tabs-boxs&&.fed-part-rows&&li",
+		"lists": ".fed-play-item;body&&ul,1&&li"
+	},
+	{
+        "id": 26,
+		"img": ".lazyload&&data-original",
+		"desc": ".play-ail&&p,-2&&Text",
+		"content": ".detail-intro-txt&&Text",
+		"tabs": "#playTab&&li",
+		"lists": ".playlist&&ul;body&&li"
+	},
+	{
+        "id": 27,
+		"img": ".v-pic&&img&&src",
+		"desc": ".txt_list&&li,2&&Text;.txt_list&&li,3&&Text;.txt_list&&li,4&&Text",
+		"content": ".infor_intro&&Text",
+		"tabs": ".play_source_tab&&a",
+		"lists": "body&&.play_num_list;body&&li",
+	},
+	{
+        "id": 28,
+		"img": ".dyimg&&img&&src",
+		"desc": ".moviedteail_list&&Text",
+		"content": "body&&.yp_context&&Text",
+		"tabs": ".mi_paly_box&&.ypxingq_t",
+		"lists": ".paly_list_btn;body&&a"
+	},
+	{
+        "id": 29,
+		"img": ".lazyload&&data-original",
+		"desc": ".myui-content__detail&&.data&&Text",
+		"content": "#rating&&Text",
+		"tabs": "body&&.myui-panel__head&&h3",
+		"tab_text": ".title&&Text",
+		"lists": "#playlist1&&li;body&&a"
+	},
+	{
+        "id": 30,
+		"img": ".m_background&&style",
+		"desc": ".v_info_box&&p&&Text",
+		"content": ".p_txt&&Text",
+		"tabs": ".from_list&&li",
+		"lists": "#play_link&&li;body&&a"
+	},
+	{
+        "id": 31,
+		"img": ".lazy&&src",
+		"desc": ".detail_top&&li,2&&Text;.detail_top&&li,,3&&Text;.detail_top&&li,4&&Text",
+		"content": ".li_intro&&Text",
+		"tabs": ".play_source_tab&&a",
+		"lists": "body&&.player_list;body&&li"
+	},
+	{
+        "id": 32,
+		"img": "#book-cont&&img&&src",
+		"desc": "#book-cont&&.r&&Text",
+		"content": "#wrap&&Text",
+		"tabs": "#zhankai&&.arconix-toggle-title",
+		"lists": "#zhankai&&.arconix-toggle-content;body&&li"
+	},
+	{
+        "id": 33,
+		"img": "#imglazy&&src",
+		"desc": ".info,0&&Text;.info,1&&Text;.info,2&&Text;.starring&&Text",
+		"content": ".animeplot&&Text",
+		"tabs": "#two1",
+		"lists": ".playlist;body&&.list-title"
+	}
+]
 
 function 获取源码(url){
     let html =request(url);
@@ -13,22 +277,17 @@ function autoerji(url){
     //log(url);
     let data = {};
     if(!/http/.test(url)){return data;}
-    try{
-        var tmpllist = JSON.parse(fetch(erjitmplfile));
-    }catch(e){
-        var tmpllist = [];
-    }
     let html = 获取源码(url, {headers: {'User-Agent': PC_UA }});
     let urldomian = url.match(/http(s)?:\/\/(.*?)\//)[0];
     let urltmpl = JSON.parse(getMyVar('Tmpl-'+urldomian,'{}'));
-    let tmplidex = tmpllist.findIndex(it=>it.名称===urltmpl.名称);
+    let tmplidex = erjiTmpl.findIndex(it=>it.id===urltmpl.id);
     if(tmplidex>-1) {
-        let tmpl = tmpllist.splice(tmplidex, 1);
-        tmpllist.unshift(tmpl[0]);
+        let tmpl = erjiTmpl.splice(tmplidex, 1);
+        erjiTmpl.unshift(tmpl[0]);
     }
-    for(let i in tmpllist){
-        log('【'+tmpllist[i].名称+'】');
-        let t = tmpllist[i].解析;
+    for(let i in erjiTmpl){
+        log('【'+erjiTmpl[i].id+'】');
+        let t = erjiTmpl[i];
         try {
             let tabs = pdfa(html,t.tabs);
             log(tabs)
@@ -40,11 +299,11 @@ function autoerji(url){
                 }
             });
             log(arts)
-            let lists = pdfa(html,'body&&'+t.lists.replace(',#id',';body').split(';')[0]);//全线路影片列表
+            let lists = pdfa(html,'body&&'+t.lists.split(';')[0]);//全线路影片列表
             log(lists)
             var conts = [];
             for (let i = 0; i < lists.length; i++) {
-                let key = t.lists.replace(',#id',';body').split(';')[1];
+                let key = t.lists.split(';')[1];
                 let list = pdfa(lists[i],key);//单线路影片列表
                 let cont = [];
                 for (let j = 0; j < list.length; j++) {
@@ -65,7 +324,7 @@ function autoerji(url){
                 break;
             }
         }catch (e) {
-            log('二级模板【'+tmpllist[i].名称+'】匹配失败：'+e.message);
+            log('二级模板【'+tmpllist[i].id+'】匹配失败：'+e.message);
         }
     }
     //log(data);
