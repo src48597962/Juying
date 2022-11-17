0aaffac1f14a1fd7ea162531f9fe3fc62f4edf86
src48597962 <48597962@qq.com>
2022-11-17 15:16:32 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -181,8 +181,8 @@ function xunmi(name,data,ishkss) {
         if(lists.length>0){
             let Cdatalist =[];
             lists.forEach(item => {
-                let vodname = item.text.replace(/<em>|<\/em>/g,'');
-                if(!/qq|mgtv|iptv|iqiyi|youku|bilibili|souhu|cctv|kkw361|ksksl|zjtu\.cc|bdys01/.test(item.url)){//&&vodname.indexOf(name)>-1
+                let vodname = item.text.replace(/<em>|<\/em>/g,'');//|kkw361|ksksl|zjtu\.cc|bdys01
+                if(!/qq|mgtv|iptv|iqiyi|youku|bilibili|souhu|cctv/.test(item.url)){//&&vodname.indexOf(name)>-1
                     let push = {
                         title: ishkss?vodname:(vodname!=name?vodname.replace(name,'<font color=red>'+name+'</font>'):vodname) + ' - <font color=#f13b66a>'+ item.website+'</font>' + (item.tags.length>0?'  ['+item.tags.join(' ')+']':''),
                         desc: item.website + (item.tags.length>0?'  ['+item.tags.join(' ')+']':'') ,
