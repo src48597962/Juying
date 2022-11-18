ef797789c3d51e4db3706db5d839cdf4f0fa6b5c
src48597962 <48597962@qq.com>
2022-11-18 23:55:44 +0800

    更新 'SrcAutoTmpl.js'

--- SrcAutoTmpl.js
+++ SrcAutoTmpl.js
@@ -135,6 +135,7 @@ function autoerji(url){
 			}else{
 				sortlist.push({id:setid,sort:1});
 			}
+			log(data.details2);
 			data = {details1:data.details1||detail.details1,details2:data.details2||detail.details2,pic:data.pic||detail.pic,desc:data.desc||detail.desc,arts:data.arts,conts:data.conts};
 		}
     }
