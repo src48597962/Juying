0f1dd3e7683b8def92024427dc6a18fb5ddb2e8d
src48597962 <48597962@qq.com>
2022-11-19 23:15:27 +0800

    更新 'SrcJyCustomJk.js'

---
+++ SrcJyCustomJk.js
@@ -0,0 +1,23 @@
+function chabeisousuo(name) {
+    let list = [];
+	eval(getCryptoJS());
+	let token = CryptoJS.SHA1(name + "URBBRGROUN").toString();
+	try {
+		let html = request('https://api.cupfox.app/api/v2/search/?text=' + name + '&type=0&from=0&size=200&token=' + token);
+		var lists = JSON.parse(html).resources;
+	} catch (e) {
+		var lists = [];
+	}
+    lists.forEach(item => {
+        let vodname = item.text.replace(/<em>|<\/em>/g, ''); 
+        if (!/qq|mgtv|iptv|iqiyi|youku|bilibili|souhu|cctv/.test(item.url)&&vodname.indexOf(name)>-1) {
+            list.push({
+                vodname: vodname,
+                vodpic: "",
+                voddesc: item.website + (item.tags.length > 0 ? '  [' + item.tags.join(' ') + ']' : ''),
+                vodurl: item.url
+            })
+        }
+    });
+    return list;
+}
