2f8a538324bf8af0aede6ff2e038c51b8df9fed8
src48597962 <48597962@qq.com>
2022-11-20 10:08:09 +0800

    更新 'SrcAutoTmpl.js'

--- SrcAutoTmpl.js
+++ SrcAutoTmpl.js
@@ -250,7 +250,7 @@ function aierji(html,url,detail){
 	for (let i = 0; i < arr.length; i++) {
 		let it = arr[i];
 		let t = it.text;
-		if (!it.href || it.href == "" || (it.href==url&&it.href.indexOf('-')==-1) || /voddetail|s\/guoyu/.test(it.href)) {
+		if (!it.href || it.href == "" || (it.href==url&&it.href.indexOf('-')==-1) || /voddetail|s\/guoyu|vodshow/.test(it.href)) {
 			continue;
 		}
 		let pre = i == 0 ? null : arr[i - 1];
