514997bf11888ba1c662c795db02f31ac810acd1
src48597962 <48597962@qq.com>
2022-11-20 20:51:04 +0800

    更新 'SrcJyCustomJk.js'

--- SrcJyCustomJk.js
+++ SrcJyCustomJk.js
@@ -41,6 +41,7 @@ let customparse = {
                     };
                     let doghtml = request('https://www.dianyinggou.com/SpiderMovie/zy/' + dogname, {headers: headers});
                     let htmls = pdfa(doghtml, "body&&a");
+                    log(htmls);
                     htmls.forEach(item=>{
                         let sitename = pdfh(item, "a&&li,1&&Text");
                         let vodname = pdfh(item, "a&&li,0&&Text");
@@ -53,6 +54,7 @@ let customparse = {
             log(e.message);
             var lists = [];
         }
+        log(lists);
         let list = [];
         let task = function(obj) {
             let trueurl = request(obj.url, {redirect: false, withHeaders: true});
