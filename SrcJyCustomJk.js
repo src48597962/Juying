5e7dcfba6e511b417bffeb3a96130a3a7924584b
src48597962 <48597962@qq.com>
2022-11-20 20:56:07 +0800

    更新 'SrcJyCustomJk.js'

--- SrcJyCustomJk.js
+++ SrcJyCustomJk.js
@@ -41,15 +41,15 @@ let customparse = {
                     };
                     let doghtml = request('https://www.dianyinggou.com/SpiderMovie/zy/' + dogname, {headers: headers});
                     let htmls = pdfa(doghtml, "body&&a");
-                    log(htmls);
                     htmls.forEach(item=>{
-                        let sitename = pdfh(item, "a&&li,1&&Text");
-                        let vodname = pdfh(item, "a&&li,0&&Text");
-                        let vodurl = pdfh(item, "a&&href");
+                        let sitename = pdfh(item, "body&&li,1&&Text");
+                        let vodname = pdfh(item, "body&&li,0&&Text");
+                        let vodurl = pdfh(item, "body&&href");
                         lists.push({name:vodname,pic:dogpic,url:vodurl,site:sitename})
                     })
                 }
             })
+            log(lists);
         } catch (e) {
             log(e.message);
             var lists = [];
