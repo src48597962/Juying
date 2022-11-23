49f4b6f29d980da0634aad148cded78957030dc5
src48597962 <48597962@qq.com>
2022-11-23 12:14:13 +0800

    更新 'SrcLive.js'

--- SrcLive.js
+++ SrcLive.js
@@ -411,9 +411,6 @@ function LiveSet() {
                 img: "https://img.vinua.cn/images/QqyC.png",
                 url: $("","输入通用格式的tv链接地址").input((livecfgfile,liveconfig)=>{
                     if(input){
-                        if(/^\/storage/){
-                            input = 'file://' + input;
-                        }
                         let livedata = liveconfig['data']||[];
                         if(!livedata.some(item => item.url==input)){
                             showLoading('正在验证链接有效性...');
