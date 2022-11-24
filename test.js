9c31edc058664c91c2dc50f8782d1098288fd2f2
src48597962 <48597962@qq.com>
2022-11-24 22:00:36 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -1,34 +1,18 @@
 js:
 let d = [];
-let Color = "#f13b66a";
-let html = request("https://getpodcast.xyz/");
-//log(html)
-let class_ids = pdfa(html, "body&&.classify_title");
-let classlists = pdfa(html, "body&&.pic_list");
-for (let i in class_ids) {
-    let classname = pdfh(class_ids[i],"body&&Text");
-    d.push({
-        title: getMyVar('SrcBoKe$classname', '播客') === classname ? '““””<b><span style="color:' + Color + '">' + classname + '</span></b>' : classname,
-        url: $('#noLoading#').lazyRule((classname, i) => {
-                putMyVar('SrcBoKe$classname', classname);
-                putMyVar('SrcBoKe$classid', i);
-                refreshPage(false);
-                return "hiker://empty";
-            }, classname, i),
-        col_type: 'scroll_button'
-    });
-}
-
-let lists = pdfa(classlists[Math.floor(getMyVar('SrcBoKe$classid','0'))], "body&&li");
+setPageTitle(MY_PARAMS.title);
+MY_URL = MY_URL.split('##')[1];
+let html = request(MY_URL);
+let lists = pdfa(html, "body&&item");
+//log(lists)
 lists.forEach(item => {
     d.push({
-        title: pdfh(item, ".title&&Text"),
-        url: "hiker://empty##" + pdfh(item, "a&&href"),
-        pic: pdfh(item, ".pic&&img&&src"),
-        col_type: 'card_pic_2'
-    });
+        title: item.match(/<title>(.*?)<\/title>/)[1],
+        desc: pdfh(item, "description&&Text"),
+        url: pdfh(item, "enclosure&&url"),
+        img: item.match(/image href="(.*?)"/)[1],
+        col_type: 'movie_1_left_pic'
+    })
 })
-      
-
 
 setResult(d);
