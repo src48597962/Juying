edd70539a9695052d999a595be8c3a14a89c8a3b
src48597962 <48597962@qq.com>
2022-12-11 13:56:06 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -78,6 +78,17 @@ let yijimenu = [
                     refreshPage(false);
                     return "toast://已切换"
                 })
+            },{
+                title: "🍭搜索模式："+(getSearchMode()==1?"精准":"默认"),
+                js: $.toString(() => {
+                    if(getSearchMode()==1){
+                        setSearchMode(0);
+                    }else{
+                        setSearchMode(1);
+                    }
+                    refreshPage(false);
+                    return "toast://已切换"
+                })
             }]
         }
     },
