6a2a8fcf4258d23aa96611626d3a28b7d259f49b
src48597962 <48597962@qq.com>
2022-12-13 10:41:17 +0800

    更新 'SrcGetParse.js'

--- SrcGetParse.js
+++ SrcGetParse.js
@@ -1,2 +1,103 @@
-
+/*
 evalPrivateJS('WzACVNoBzFOq5taXZgG/h/v+lCYk2AlD1xaVPdB4NgrM50Ov8nxy8/nnhXrQyPUupkD0weCrBbrvuGmg5BYEWJP3yYamGcmMEjzCHwuT3NJ2NcS1/z2uSPJDpyEf01htyHgczoiWCMGpEE5QKyuLVKl0DwnSsySMJ03zy2PVDTj/DVzIn5tji6SRzRsKeUZn1exbFFuDxsNPA1RIyKy6FUxk7zQg5T8p2J3zcGfsciuWTWfurXvDJkxGtL6XOqGR36rGk9qViUEMRMSDCSZVMBy2cSl5Zt3v7qnT9vDoPFwL7CPmmnKSde/unXBCsGeWkyyINITb5EUfshkCVM8S54T9znegu17iB/hq1kPT8mUVR88JpKzsw7bf7fz6CjvUvMKlfTlc9yrMz+p+aOnjc/+PWYdG7y2l39voegDAHKQFPgP+AZbjDRHOLk2KblcVyQsaWih9IxoXJ/xJ/W8gl3pjc/NvYF2ItAxUZcFKjFS0gSPwrSt1SZ8xTmIqjMp2AYL2WacABqz1NQzp4e6/9Gqc1gQMRSBLRNZV/xQV3iC/kPlxBNcDKV00fwAb3Yszufq3nrydB9he+TJbBbOoCKe3bgkzy2hUaLpodaV4ym5W5UlpnVpn61AIFuURP1gMx/GdZRs+wwsEVbGLkfpfvoimASQtdaLOrtGQnFZmB9c9H5QjWZ8649DKthb5rL2P3ww1WFxDFuh7AO+TheQV6LpKDUSm0Qc8kuHJjvo8cYQsdDFD8nEP7100pmnucX6Kh6nL5Q8mp7LepoNYqFbXNwwSDpqY7oT8ONEmx8Sf0h6mL8pIaC443QVUgyumnk5154uptQHSw8NXBzDJTQ/uRTfiORzAb6OhG9PgXTlJq7vIZGa6uJOK8lwmGNZYDc7UKIhiyObwUt34slZyDJQvaRlRZRcUQWvSNK1znBVdaNA6PMT5Kcx69lmnk69V5qnVn0Mbqp2rqsqplCJKE5QOByrZ3bG8ofTcQnhimhusrj7iV/dRUwbQvDizbe9K62sMNdAtyjpxrmhHpDwmBlR8ZCjvDD6U6E2oymhBwPNnICqGdoWc/aWgAPeuckQkU+hUVD3Z+Q2vGfXOBofDVPmXrYZ2hZz9paAA965yRCRT6FTBc7qq1TcbVTzBoazycv9/hnaFnP2loAD3rnJEJFPoVOGHgv+sIXeH5L/PH2oRADv3ngAD50n1ay80YEUYysoBF+0yHLl7rixKU47WgwSTW06lAmdhjSIUh6FK01zFFOyxOqceHGsCeAQKma9XDEiDwNn9Wg11DHsagUf0evCgN/WX4K8vWy/XXNFI8XDk8tdpi2/iaoeOJCazUzhcpa6XfNIaP6SBlpcWXt0kd96nY5+orwCJGjd79y2+HkFwtyhKh8foWlNMDxkDSdfWjmTabCA4w7eOUz5UE5l18eVgDe3v1SWEFUZjgO7/d8vq436xA4b5HlRZ51ervnH2vJ8R8lHIJ/sj9IC1lhWS3z71u9HR95v0fTnc9pNrjD4q8XqU9QY1XX23oEyfIbctim9aJOPRhomFa7v9XJnHy3KPmNjE9H/WAkLvyr6FSTzMAavnH9nJ1rXvTkOuEnWhPeqa+nuDvad0kFZQnE2RMWaszUnfYOrq/yqUPFbB8E+ACvJv4GSD7pO7Ri5N7jZBwOl15Ey+Vg+DRoW1L27O6Df+C87R9/GoBvSHwOYOCNamIdSNwAgxkd12KzZi6cHjkFqkhk4gEN8CsydkcD8wELv+aER11Kp7AgI2nFmFulSQKmw6vZ7xIPq6QCvhV4KSQjktV8xX9WBNdFLsm8rKXRrdYFlKq4Czgg0vL6bRCksfm61jPRKT/B3hJbBoZONTAfRkhBsdcyS4HSvleG9LWipqZlM9BuZD2YZv8qdWbmshvGvqk+46p3ZOO74CUhIgnMZfDar8dFy6VbkRxMLXsuCgmFaYylIDt/iT2dTKbbhQx+yILRA2xgy9BZOivLIJ8pc7h7CB1bZf10kRi61An2i0KQwVl0dg04+/5IJySSKXCUqXrlMfN3HtbszXf945I03oCz+x3O8adz60LDpZeJPnQ+mPsBMTutGWMak6y+iSBJ5xUhb5ljOt/7QkRmw7o0EtFXqWj6ERfd625lRT2wSj5c41W8S88VjO13MNDVE+0yJp/piltzOYxilodPJ9rzmm6XLTFAGA4b1/38QLhXaaGhnB58mndfASx04WdySv96eytKpTeJbzutuAqi6ZHnxrJha+R+KvdzC2vkVaCzcyv9lzw7l8s3L8pfYSTl51oPZe4RFdhK7xMHMIk7z8pxQeHumoKT0ftWXSTLkfiq+OcM6UMvsO/POw5Ccr79sJzic8olSvrwrZQDhJOaUqLC0/zpQy+w7887DkJyvv2wnOJ8ry0VQXf19/duQSbbFE86t6riu5NW2+torKNK8r+FreX6qeTh54MTQ8PAEV0iVP6l41+1iuAV1smzuKuIUNBGbXWRCKSTxHCTCTzb5WV3FhyyvLSNZWc/sIn/vWSAHU9iT/Wuh+az4OICDhBEiT3/IxHbRM05Fo6eYyx/AxbZUltvRET6xNvWVC5hkw1FikRtCwZi18PeeBrQzL5/U9RYeKVWELYGndh8T/Y+sNVMQ+qBBTt0C670yL2Ddz7WzKY6SMf3+O05Cyj9fSTmyrJiN/+cpYsKLGN5Rj1B99Tfm/sDubJRX6IDZPzj8twQ6ScnWSa4BnONkXXDy5VF/wr0tlWmrYssz1Cz1HW3JNu6Qw2tfti+SaiwLjX6MWsCEdWIjWmFCQBenNlMEQMTPQgs+B9WYQP9q2rbmeSirT9c1yakGAELli86AJ1MXoYpJPBpFKx0WjCDC4e02BhIvpqtz23dyRm0Udg9vECbY9o0vyxPz//BvcAeCrq84ueTtrepvEJQeoez49Qhk1YJ3gnyEg3tKnF6XCT7wohSePtEGkoFupZPnevlSo8BrxhJLXMBMUha8vQER5IWWAAwTeWuu43KLi3CflhoD7x8Zr3IucgRzRe1pIsJH/JWwfUc7Rm+ElWhhWSUjfcrJTDTDLA1HoT0jJMKwHSc7Ab/uQeRyyrLSUdWCAtXzh091YkBM//arLq4jOiZhgeOOEIVGHZrmK5rupc1DPblPZ1jopKePgCHn9y+KEDAWmtto2Eg6M1VeEz+K5T9cqldUwHZUtLZ9fb91UIDC9nJxEMqZfX52vK81ExjlHlRNlq3p8lUZZJ8XAnCQVuIif/XHYm2VOsrlVLmdA8ls27kgSj68Bqdtet7+bBITOozj7F0XHOfMhcpdGFJQThMVKojkSCL0oz9j5qiK8Kf6Utolz3gd56rT5yFGGg9AsBtzpIwOJYLjyh3fbDFhb87xLS1TaV+RZm8/gqseYkEJZUJzefnRRCh3mbgdRPkxGAgmXfPKQV6qCeNNEwcb6/oHc6e6UV24JXDBWwHtWa1d85OdkBj2fxMZB7wB71rbg0yZmkT9oR7xiEOonzreKMlsD+UB5r5Y7yj2LALfTlBTPQFgU0T3t7Or9RPnUwmFah+TTvE8qGLa8or9ndeA126QbEcVRkpjCDz+S9Lt0/HMp8F/G2zdJ0aHkWjuhw8PZVcvDnrHxcxagydqWXXtJX2fr7XIUNE3L2vGaVOzg555dwyw2Kai3iIQ9qbRdbyR/WYy/5QlT52O0hB9ckIq3V6F2v9VCvGFuzzH9cwZjL7qc1RPNIzcXMEreyyvLSNZWc/sIn/vWSAHU9ljeKnMiFbIVUoq54L6WJ9njdDiQptiKrCSZx1yXdX68wI+k1Mpbw8OWFStqL2qOqbBIdRyAFx3JU3BA+AZElJiQCNK/iHXueWTb0ysYbQRazpQy+w7887DkJyvv2wnOJ2aLRT+oiTNGghZFDSA5PETXJFbV72BQljwUK96GV3aQwHelD1loEesCyD501H7RYwynHogcXpw2ZsY/sti7hEqtviEu8toWPNyfRdiikj2WmlTs4OeeXcMsNimot4iEPREypQSs0xqX/hpik+IBsLAVzZdoA9i+QzWWcHHuJgZX/XMGYy+6nNUTzSM3FzBK3lM9BuZD2YZv8qdWbmshvGufreA+sglne50UsYQCIYwDjBD0F373d8zEICGMe5S4VLg2C12aSTL//WWKnRLJIxz+wuYmmecmy+MlEpt1dMDarMcd9qg1ksPasWXpILSj29x1LprvC4eYIp/wFBCGFYnkdH8vH+xVcVAMBap6dj1hNry/FZMGYbYRCj5Oerva2FmhD8sV+H4m1utJORy5VHvQ5lZROpwWqL+VJ1Dilfv6c3Q2KLVcr9ZwnkcqUXv//bzRtoHilNCrTz79lfuJC2EOJm96QxE96tBkeGS/nBjbs9E/EiRQA44V0+NoDdH6kuiEqYpphUF5+0fCL7Yi+A6f4PpXEUhgzsbR2neTYwTlah3nzJNwoBvsf1O7IVvppTznjH1PFGppniCG6M0X70tlWpAU5XsLjaWenxYpxfCY62kSrJ08vUPg6+iWk/Iedw9GyydG5fckfxkgPvt1qTq20b7casBygibwxeBlaJAafHwMkdmqm27Y5shJ+TeMGh/jvzmf+Pgq043vrn7QYGs=')
+*/
+let 取随机列表 = function (arr, num) {
+    let sData = arr.slice(0), i = arr.length, min = i - num, item, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        item = sData[index];
+        sData[index] = sData[i];
+        sData[i] = item;
+    }
+    return sData.slice(min);
+};
+function 取随机用户名(num) {
+    num = num || 6;
+    let arr = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    return 取随机列表(arr, num).join("");
+}
+function 取随机QQ号(num) {
+    num = num || 6;
+    let arr = "0123456789".split("");
+    return 取随机列表(arr, num).join("");
+}
+
+function 注册账号(regurl,regdata) {
+    let reg = request(regurl,
+                {headers: {'Referer': regurl },
+                body: regdata,
+                method: 'POST',
+                redirect:false,
+                withHeaders:true,
+                timeout: 3000
+            });
+
+    let regjg = JSON.parse(reg).headers.location || "";
+    return regjg;
+}
+let username = 取随机用户名(10);
+let qq = 取随机QQ号(10);
+let 获取解析 = {
+    nxflv: function() {
+        let regurl = "https://vip.nxflv.com/user/login/reg";
+        let regdata = "username="+username+"&qq="+qq+"&password="+username;
+        let result = 注册账号(regurl,regdata);
+        if(/success/.test(result)){
+            //fetch(loginurl, {timeout: 5000});
+            let loginurl = "https://vip.nxflv.com/user/login";
+            //request(loginurl, {timeout: 5000});
+            let login = request('https://vip.nxflv.com/user/login/checkUser',
+                {headers: {'Referer': loginurl, 'Cookie': getCookie(loginurl)},
+                body: "username="+username+"&password="+username,
+                method: 'POST',
+                timeout: 3000
+            });
+            if(JSON.parse(login).status == 200){
+                let html = request("https://vip.nxflv.com/user/information", {headers: {'Referer': loginurl, 'Cookie': getCookie(loginurl)}});
+                let uid = html.split('<input type="number" class="form-control" value="')[1].split('"')[0];
+                let key = html.split('<input type="text" class="form-control" value="')[2].split('"')[0];
+                return "https://json.nxflv.com/?uid="+uid+"&key="+key+"&url=";
+            }
+        }
+        return "";
+    }
+}
+function bpParse(vipUrl,parsename) {
+    try{
+        if(!/聚影|帅助手/.test(MY_RULE.title)){
+            log('非法白嫖');
+            return "";
+        }
+        
+        let parsefile = readFile('hiker://files/rules/Src/bpParse.json');
+        if(parsefile){
+            var parsejson = JSON.parse(aesDecode('Juying', parsejson));
+        }else{
+            var parsejson = {};
+        }
+        function getparse(parse){
+            parse = 获取解析[parsename]();
+            if(parse){
+                parsejson[parsename] = parse;
+                saveFile('hiker://files/rules/Src/bpParse.json', JSON.stringify(aesEncode('Juying', parsejson)));
+            }
+            return parse;
+        }
+
+        let parse = parsejson[parsename]?parsejson[parsename]:"";
+        parse = parse?parse:getparse(parse);
+        if(parse){
+            let json = JSON.parse(request(parse + vipUrl, {timeout: 5000 }));
+            if(json.code==300){
+                parse = getparse(parse);
+                json = JSON.parse(request(parse + vipUrl, {timeout: 5000 }));
+            }
+            return json.url || "";
+        }else{
+            return "";
+        }
+    }catch(e){
+        log(e.message);
+        return "";
+    }
+}
