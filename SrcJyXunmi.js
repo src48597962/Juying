9517defe3a2095a49405ca637a8b1dc045e2a949
src48597962 <48597962@qq.com>
2023-02-04 13:42:34 +0800

    更新 'SrcJyXunmi.js'

--- SrcJyXunmi.js
+++ SrcJyXunmi.js
@@ -1071,8 +1071,10 @@ function xunmierji(type,ua) {
                     conthtml = conthtml.split(jkdata["播放二次截取"].split('&&')[0])[1].split(jkdata["播放二次截取"].split('&&')[1])[0];
                 }
                 let contlist = conthtml.match(new RegExp(jkdata["播放数组"].replace('&&','((?:.|[\r\n])*?)'), 'g'));
+                log(contlist);
                 for (let i = 0; i < contlist.length; i++) {
                     let bfline = jkdata["播放列表"]?contlist[i].match(new RegExp(jkdata["播放列表"].replace('&&','((?:.|[\r\n])*?)'), 'g')):pdfa(contlist[i],"body&&a");
+                    log(bfline);
                     let cont = [];
                     for (let j = 0; j < bfline.length; j++) {
                         let contname = jkdata["播放标题"]?bfline[j].split(jkdata["播放标题"].split('&&')[0])[1].split(jkdata["简播放标题介"].split('&&')[1])[0]:pdfh(bfline[j],"a&&Text");
