0d34d530a140838b102c8342996ec3de769cf156
src48597962 <48597962@qq.com>
2023-02-15 16:23:55 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -101,7 +101,7 @@ function alistlist(alistapi){
   let d = [];
   let listid = base64Encode(MY_PARAMS.path);
   d.push({
-    title: "<span style='color: #99ff66'><b>🏠</b>"+ (MY_PARAMS.path||"") + "</span>",
+    title: "<span style='color: #99ff66'>📑📁"+ (MY_PARAMS.path||"") + "</span>",
     col_type: 'rich_text'
   })
   d.push({
