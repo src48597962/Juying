3b962e2e66adad7cbb0dcc3ce3b97911600016d2
src48597962 <48597962@qq.com>
2023-02-17 15:18:43 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -44,10 +44,12 @@ function getlist(data,isdir) {
         return isdir ? item.is_dir : fileFilter?contain.test(item.name):item.is_dir==0;
     })
     list = list.sort((a,b)=>{
+        let oDate1 = new Date(a.modified);
+        let oDate2 = new Date(b.modified);
         if(a.modified!=b.modified){
-            return a.modified - b.modified
+            return oDate2.getTime() > oDate1.getTime()
         }else{
-            return a.name - b.name;
+            return b.name - a.name;
         }
     });
     log(list);
