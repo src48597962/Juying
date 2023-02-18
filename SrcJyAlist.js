00016dfdfe48bec9415756b5c7d3c32df007f7bc
src48597962 <48597962@qq.com>
2023-02-18 11:40:15 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -208,7 +208,6 @@ function arrayAdd(list,isdir,alistapi){
 
 function alistUrl(api,path,pwd,sign) {
   if(contain.test(path)){
-    return api + "/d"+ path + "?sign=" + sign;
     try{
       let json = JSON.parse(gethtml(api + "/api/fs/get", path, pwd));
       if(json.code==200){
@@ -223,10 +222,10 @@ function alistUrl(api,path,pwd,sign) {
   }
 }
 
-function alistSearch(api,key) {
+function alistSearch(alistapi,key) {
   deleteItemByCls('alist');
   try{
-    let json = JSON.parse(fetch(api + "/api/fs/search", {headers:{'content-type':'application/json;charset=UTF-8' },body:{"per_page":100,"page":1,"parent":"/","keywords":key},method:'POST',timeout:10000}));
+    let json = JSON.parse(fetch(alistapi.server + "/api/fs/search", {headers:{'content-type':'application/json;charset=UTF-8' },body:{"per_page":100,"page":1,"parent":"/","keywords":key},method:'POST',timeout:10000}));
     log(json);
     if(json.code==200){
       let dirlist = getlist(json.data.content,1);
