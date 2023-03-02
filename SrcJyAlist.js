79d53445a272bd36b882f5397fb55a1fdd3094ab
src48597962 <48597962@qq.com>
2023-03-02 12:52:12 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -13,10 +13,14 @@ let audiovisual = alistconfig.contain||'mp4|avi|mkv|rmvb|flv|mov|ts|mp3|m4a|wma|
 let contain = new RegExp(audiovisual,"i");//设置可显示的影音文件后缀
 let music = new RegExp("mp3|m4a|wma|flac","i");//进入音乐播放器
 let image = new RegExp("jpg|png|gif|bmp|ico|svg","i");//进入图片查看
+let suffixcontain = new RegExp(audiovisual,"i");//设置可显示的影音文件后缀
 
 function getlist(data,isdir,filter,audiovisual) {
     let contain = new RegExp(audiovisual,"i");//设置可显示的影音文件后缀
     let list = data.filter(item => {
+      if(!isdir){
+        log(item.name+'>'+suffixcontain.test('mp4'))
+      }
         let suffix = item.name.substring(item.name.lastIndexOf('.')+1);//后缀名
         return isdir ? item.is_dir : filter? (contain.test(suffix) || /srt|vtt|ass/.test(suffix)) : !item.is_dir;
     })
