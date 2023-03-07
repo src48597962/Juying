9d0bc1ce0e1fd1cde57f7e0fd40d0b7c6502b490
src48597962 <48597962@qq.com>
2023-03-07 09:49:57 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 eval(getCryptoJS());
-const curve = CryptoJS.ECDSA.curveNames.p256;
+const curve = {
+  curve: CryptoJS.ECDSA.getCurveByName('secp256r1'),
+  G: null,
+  n: null,
+  h: null
+};
 const appId = '5ddexxxbdf9e4966b387ba58f4b3fdc3';
 const deviceId = '2eexxx84b9fc4860b9427feb97a4c142';
 const userId = '3426axxxbaa04e1ea9ee01bd998d06d4';
