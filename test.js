707b8d5c67c3c770a48c8d8edbfce7e8bcb38775
src48597962 <48597962@qq.com>
2023-03-07 09:59:39 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const b = BigInt(7);
 const Gx = BigInt('0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798');
 const Gy = BigInt('0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8');
 const n = BigInt('0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141');
-const h = 1n;
+const h = BigInt(1);
 // 定义点类
 function Point(x, y) {
   this.x = x;
