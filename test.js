fec98ef1b2c7a9ab69a2f89e0a9e611b92e9320c
src48597962 <48597962@qq.com>
2023-03-07 09:47:10 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -1,36 +1,27 @@
 eval(getCryptoJS());
-function sha256(str) {
-  const data = new Uint8Array(Array.from(str, c => c.charCodeAt(0)));
-  return CryptoJS.subtle.digest('SHA-256', data).then(hashBuffer => {
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-    return hashHex;
-  });
-}
-function getRandomBytes(len) {
-  const arr = new Uint8Array(len);
-  CryptoJS.getRandomValues(arr);
-  return Array.from(arr, (dec) => ('0' + dec.toString(16)).substr(-2)).join('');
-}
-function toHex(buf) {
-  return Array.prototype.map.call(new Uint8Array(buf), x => ('00' + x.toString(16)).slice(-2)).join('');
-}
-function fromHex(hex) {
-  return new Uint8Array(hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-}
-const curve = 'P-256';
+const curve = CryptoJS.ECDSA.curveNames.p256;
 const appId = '5ddexxxbdf9e4966b387ba58f4b3fdc3';
 const deviceId = '2eexxx84b9fc4860b9427feb97a4c142';
 const userId = '3426axxxbaa04e1ea9ee01bd998d06d4';
 let nonce = 0;
-const keyPairPromise = CryptoJS.subtle.generateKey(
-  {
-    name: 'ECDSA',
-    namedCurve: curve
-  },
-  true,
-  ['sign', 'verify']
-);
+// 生成密钥对
+const keyPair = CryptoJS.ECDSA.generateKeyPair(curve);
+function sha256(str) {
+  const hash = CryptoJS.SHA256(str);
+  return hash.toString(CryptoJS.enc.Hex);
+}
+function getRandomBytes(len) {
+  const arr = new Uint8Array(len);
+  CryptoJS.lib.WordArray.random(len).toArray().forEach((val, idx) => arr[idx] = val);
+  return Array.from(arr, (dec) => ('0' + dec.toString(16)).substr(-2)).join('');
+}
+function toHex(buf) {
+  return Array.prototype.map.call(buf, x => ('00' + x.toString(16)).slice(-2)).join('');
+}
+function fromHex(hex) {
+  return new Uint8Array(hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+}
+
 toast('加载完')
 
 setResult(d);
