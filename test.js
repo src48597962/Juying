d6fcdd853ca99dcc06a7b8a7ce09c8102db9a4b8
src48597962 <48597962@qq.com>
2023-03-07 09:29:21 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -1,66 +1,43 @@
 function generateKeyPair() {
+  // Generate a random 256-bit private key
   const privateKey = new Uint8Array(32);
   crypto.getRandomValues(privateKey);
+  // Derive the corresponding public key
   const curve = {
-    p: hexToBigInt('0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f'),
-    a: hexToBigInt('0x0000000000000000000000000000000000000000000000000000000000000000'),
-    b: hexToBigInt('0x0000000000000000000000000000000000000000000000000000000000000007'),
-    n: hexToBigInt('0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141'),
-    Gx: hexToBigInt('0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798'),
-    Gy: hexToBigInt('0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8')
+    p: '0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f',
+    a: '0x0000000000000000000000000000000000000000000000000000000000000000',
+    b: '0x0000000000000000000000000000000000000000000000000000000000000007',
+    n: '0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
+    Gx: '0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798',
+    Gy: '0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8'
   };
-  const G = {
-    x: curve.Gx,
-    y: curve.Gy
-  };
-  const publicKey = pointMultiply(G, privateKey);
-  return {
-    privateKey: privateKey,
-    publicKey: '04' + publicKey.x.toString(16, 64) + publicKey.y.toString(16, 64)
-  };
+  const ecparams = getSECCurveFromHex(curve);
+  const curvePt = ecparams.getG().multiply(BigInteger.fromByteArrayUnsigned(privateKey));
+  const publicKeyBytes = curvePt.getEncoded(false);
+  const publicKeyHex = bytesToHex(publicKeyBytes);
+  const publicKey = '04' + publicKeyHex;
+  return { privateKey:privateKey, publicKey:publicKey };
 }
-function hexToBigInt(hex) {
-  return BigInt('0x' + hex.slice(2));
+function getSECCurveFromHex(curve) {
+  const p = new BigInteger(curve.p, 16);
+  const a = new BigInteger(curve.a, 16);
+  const b = new BigInteger(curve.b, 16);
+  const n = new BigInteger(curve.n, 16);
+  const Gx = new BigInteger(curve.Gx, 16);
+  const Gy = new BigInteger(curve.Gy, 16);
+  const curveParams = new ECCurveFp(p, a, b);
+  const curve = curveParams.getCurve();
+  const generator = new ECPointFp(curve, curve.fromBigInteger(Gx), curve.fromBigInteger(Gy));
+  const secp256k1 = new X9ECParameters(curveParams, generator, n);
+  return secp256k1.getCurve();
 }
-function pointMultiply(G, d) {
-  let Q = null;
-  let k = 1;
-  let k2 = 0;
-  while (d > 0) {
-    if (d & 1) {
-      if (Q === null) {
-        Q = G;
-      } else {
-        Q = pointAdd(Q, G, curve);
-      }
-      k2 = k;
-      k = 0;
-    }
-    G = pointAdd(G, G, curve);
-    d >>= 1;
-    k += k;
+function bytesToHex(bytes) {
+  const hex = [];
+  for (let i = 0; i < bytes.length; i++) {
+    hex.push((bytes[i] >>> 4).toString(16));
+    hex.push((bytes[i] & 0xf).toString(16));
   }
-  if (Q === null) {
-    Q = pointMultiply(G, 0n);
-  } else {
-    Q = pointMultiply(Q, k2);
-  }
-  return Q;
-}
-function pointAdd(P, Q, curve) {
-  const m = ((P.y - Q.y) * modInverse(P.x - Q.x, curve.p)) %!c(MISSING)urve.p;
-  const x = (m * m - P.x - Q.x) %!c(MISSING)urve.p;
-  const y = (m * (P.x - x) - P.y) %!c(MISSING)urve.p;
-  return { x, y };
-}
-function modInverse(a, m) {
-  a = a %!m(MISSING);
-  for (let x = 1n; x < m; x++) {
-    if ((a * x) %!m(MISSING) === 1n) {
-      return x;
-    }
-  }
-  return 1n;
+  return hex.join('');
 }
 const keyPair = generateKeyPair();
 console.log('Private key:', keyPair.privateKey);
