edf3f34a2b48561f6f33bb69d5e460cb54e2a28b
src48597962 <48597962@qq.com>
2023-03-11 21:37:44 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -972,10 +972,10 @@ function getAliUrl(share_id, file_id, alitoken) {
             }
 
             let url = base64Decode(MY_PARAMS.url);
-            log(url);
+            //log(url);
             let playUrl;
             if(url.includes(".ts")){
-              let expires = url.split('x-oss-expires=')[1].split('&')[0];
+              //let expires = url.split('x-oss-expires=')[1].split('&')[0];
               //if(Date.now()>new Date(expires)){
               //  log('过期更新')
               //  //geturl();
@@ -1002,7 +1002,7 @@ function getAliUrl(share_id, file_id, alitoken) {
 
             }else{
               log('首次更新')
-              geturl();
+              return geturl();
             }
 
               
