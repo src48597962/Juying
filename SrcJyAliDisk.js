285d3e1df231750352ce4fa9b24f67496858b09f
src48597962 <48597962@qq.com>
2023-03-16 22:40:11 +0800

    更新 'SrcJyAliDisk.js'

--- SrcJyAliDisk.js
+++ SrcJyAliDisk.js
@@ -150,17 +150,15 @@ function aliShareSearch(input) {
                 };
                 let home = "https://www.aliyundrive.com/s/";
                 if(item.url.includes(home)){
-                    let share_id = item.url.replace(home, '').replace('/folder/','');
-                    let getis = JSON.parse(request("https://api.aliyundrive.com/adrive/v3/share_link/get_share_by_anonymous",{headers: {referer: "https://www.aliyundrive.com/"}, body: {"share_id": share_id}, method: 'POST', timeout: 3000 })).file_infos || [];
-                    if(getis.length>0){
-                        arr.url = $("hiker://empty##").lazyRule((input) => {
-                            return $("hiker://empty##").rule((input) => {
-                                require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJyAliDisk.js');
-                                aliShareUrl(input);
-                            },input);
+                    //let share_id = item.url.replace(home, '').replace('/folder/','');
+                    //let getis = JSON.parse(request("https://api.aliyundrive.com/adrive/v3/share_link/get_share_by_anonymous",{headers: {referer: "https://www.aliyundrive.com/"}, body: {"share_id": share_id}, method: 'POST', timeout: 3000 })).file_infos || [];
+                    //if(getis.length>0){
+                        arr.url = $("hiker://empty##").rule((input) => {
+                            require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJyAliDisk.js');
+                            aliShareUrl(input);
                         },item.url);
                         searchlist.push(arr);
-                    }
+                    //}
                 } else if (obj.erparse) {
                     arr.url = $("hiker://empty##").lazyRule((url,erparse) => {
                         eval('let Parse = '+erparse)
