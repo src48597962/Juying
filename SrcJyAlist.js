517e2f171173cb8b7b5af5c2cc911fa14e94990a
src48597962 <48597962@qq.com>
2023-03-20 11:40:43 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -390,7 +390,7 @@ function alistUrl(alistapi,path,sign,subtitle) {
             let rurl = redirect.headers.location[0];
             let share_id = rurl.split('&sl=')[1].split('&')[0];
             let file_id = rurl.split('&f=')[1].split('&')[0];
-            let alitoken = alistconfig.alitoken;
+            //let alitoken = alistconfig.alitoken;
             let play = getAliUrl(share_id,file_id,alitoken);
             if(play.urls){
               if(subtitle){
