6ba566c6d1036b50f4a515bb78797f283fa6c678
src48597962 <48597962@qq.com>
2023-03-25 12:11:02 +0800

    更新 'test.js'

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function(input) {
     headers.authorization = "Bearer " + userinfo.access_token;
     let drive_id = userinfo.default_drive_id;
     let postdata = {"drive_id":drive_id,"limit":100,"query":"name match \""+input+"\" and type = \"folder\"","image_thumbnail_process":"image/resize,w_200/format,jpeg","image_url_process":"image/resize,w_1920/format,jpeg","video_thumbnail_process":"video/snapshot,t_1000,f_jpg,ar_auto,w_300","order_by":"updated_at DESC"}
-    let list = JSON.parse(request('https://api.aliyundrive.com/adrive/v3/file/search', { headers: headers, body: postdata, method: 'POST', timeout: 3000 }));
+    let list = JSON.parse(request('https://api.aliyundrive.com/adrive/v3/file/search', { headers: headers, body: postdata, method: 'POST', timeout: 3000 })).items;
     let data = list.map(item => {
         return {
             title: item.name,
