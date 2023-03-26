079867dbb9f94e4ad5c5ecea600e88d894595f61
src48597962 <48597962@qq.com>
2023-03-26 16:33:10 +0800

    更新 'SrcJyAliDisk.js'

--- SrcJyAliDisk.js
+++ SrcJyAliDisk.js
@@ -121,7 +121,7 @@ function aliShare(share_id, folder_id, share_pwd) {
     }))
 }
 
-function aliShareSearch(input) {
+function aliYunSearch(input) {
     showLoading('搜索中，请稍后...');
     let filepath = "hiker://files/rules/Src/Juying/yundisk.json";
     let datafile = fetch(filepath);
