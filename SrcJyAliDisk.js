464b3a5d07bb4d5ce3bf47f10fe894d443b84771
src48597962 <48597962@qq.com>
2023-03-27 19:30:45 +0800

    更新 'SrcJyAliDisk.js'

--- SrcJyAliDisk.js
+++ SrcJyAliDisk.js
@@ -149,10 +149,10 @@ function aliMyDisk(folder_id,nofilter) {
                             alistData.config = alistconfig;
                             fy_bridge_app.writeFile(alistfile, JSON.stringify(alistData));
                             localStorage.clear();
-                            alert('TOKEN获取成功！');
-                            fba.parseLazyRule(`hiker://empty@lazyRule=.js:back(true);`);
-                            //fy_bridge_app.back();
-                            return;
+                            //alert('TOKEN获取成功！');
+                            //fba.parseLazyRule(`hiker://empty@lazyRule=.js:back(true);`);
+                            fy_bridge_app.back();
+                            //return;
                         } else {
                             token_timer();
                         }
