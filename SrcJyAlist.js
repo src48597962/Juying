a106a3bcc676060db6f33e082918b1fa9bc56b46
src48597962 <48597962@qq.com>
2023-03-27 12:08:54 +0800

    更新 'SrcJyAlist.js'

--- SrcJyAlist.js
+++ SrcJyAlist.js
@@ -1,6 +1,7 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
 //引入Ali公用文件
 require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJyAliPublic.js');
+let datalist = alistData.drives || [];
 
 function getlist(data,isdir,filter) {
     let list = data.filter(item => {
