496c41ced4b05a16b43f6a99377929cd9b867f24
src48597962 <48597962@qq.com>
2023-06-29 21:55:09 +0800

    更新 'SrcJyCustomJk.js'

--- SrcJyCustomJk.js
+++ SrcJyCustomJk.js
@@ -1,25 +1,6 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
 let customparse = {
     csp_custom_aidog: function (name) {
-        let filepath = "hiker://files/rules/Src/Juying/jiekou.json";
-        let datafile = fetch(filepath);
-        if(datafile != ""){
-            eval("var datalist=" + datafile+ ";");
-        }else{
-            var datalist = [];
-        }
-        let is =0;
-        for(let i=0;i<datalist.length;i++){
-            if(datalist[i].url=="csp_custom_aidog"){
-                datalist[i].data.ext = "https://raw.iqiq.io/src48597962/hk/master/SrcJyCustomJk.js";
-                is =1;
-                break;
-            }
-        }
-        if(is==1){
-            writeFile(filepath, JSON.stringify(datalist));
-        }
-
         try {
             var lists = [];
             let html = request("https://www.dianyinggou.com/so/" + name);
@@ -91,13 +72,13 @@ let customparse = {
     csp_custom_zhuiyingmao: function(name) {
         try {
             var lists = [];
-            let html = request("https://zhuiyingmao.com/index.php/ajax/suggest?mid=1&wd="+name+"&limit=10" );
+            let html = request("https://zhuiyingmao2.com/index.php/ajax/suggest?mid=1&wd="+name+"&limit=10" );
             let data = JSON.parse(html).list;
             let cook = getCookie('https://zhuiyingmao.com');
             data.forEach(item => {
                 let maoname = item.name;
                 if (maoname == name) {
-                    let maourl = 'https://zhuiyingmao.com/voddetail/' + item.id+".html";
+                    let maourl = 'https://zhuiyingmao2.com/voddetail/' + item.id+".html";
                     let maopic = item.pic;
                     let headers = {
                         "User-Agent": MOBILE_UA,
@@ -161,45 +142,42 @@ let customparse = {
         }
         return list;
     },
-    csp_custom_aiwandou: function (name) {
+    csp_custom_77: function (name) {
+        let list = [];
         try {
-            var lists = [];
-            let html = request("https://wuli.api.bailian168.cc/movie/getsearchlist/keywords/"+name+"/page/1/rows/15.json");
+            let html = request("https://api.tyun77.cn/api.php/provide/searchVideo?searchName="+name);
             let data = JSON.parse(html).data;
             data.forEach(item=>{
-                let ainame = item.movie_name;
-                if(ainame == name || (getSearchMode()==0&&ainame.indexOf(name)>-1)){
-                    let aiurl = "https://www.wandou.pro/detail/"+item.movie_id;
-                    let aipic = item.movie_img_url;
-                    let aihtml = request(aiurl);
-                    let htmls = pdfa(aihtml,"body&&.c-kbddDX&&a");
-                    htmls.forEach(it=>{
-                        try{
-                            let sitename = pdfh(it,"span&&Text");
-                            let vodurl = pdfh(it,"a&&href");
-                            if(!lists.some(ii => ii.url==vodurl)){
-                                lists.push({name:ainame,pic:aipic,url:vodurl,site:sitename});
-                            }
-                        }catch(e){}
+                let dataname = item.videoName;
+                if(dataname == name || (getSearchMode()==0&&dataname.indexOf(name)>-1)){
+                    list.push({
+                        vodname: dataname,
+                        vodpic: item.videoCover.replace(/http.*?\?url=/,''),
+                        voddesc: item.msg,
+                        vodurl: item.id
                     })
                 }
             })
-        } catch (e) {
-            log(e.message);
-            var lists = [];
-        }
+            return {
+                list: list,//搜索结果列表
+                urlparse: function (url) {//影片链接解析处理
 
-        let list = [];
-        lists.forEach(item=>{
-            if(!/qq|mgtv|iptv|iqiyi|youku|bilibili|souhu|cctv|icaqd|cokemv|mhyyy|fun4k|jpys\.me|31kan|37dyw|kpkuang/.test(item.url)&&!list.some(ii => ii.vodurl==item.url)){
-                list.push({
-                    vodname: item.name,
-                    vodpic: item.pic.replace(/http.*?\?url=/,''),
-                    voddesc: item.site,
-                    vodurl: item.url
-                })
+                },
+                erjiparse: {//二级解析代码
+                    detail: function (html) {//影片详情
+
+                    },
+                    line: function (html) {//线路
+
+                    },
+                    list: function (html) {//选集
+
+                    }
+                }
             }
-        })
+        } catch (e) {
+            //log(e.message);
+        }
         return list;
     }
 }
