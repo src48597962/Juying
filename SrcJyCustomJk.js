7d64e00d000f41f74ff55036285bc3d9ccd628ce
src48597962 <48597962@qq.com>
2023-07-01 11:14:14 +0800

    更新 'SrcJyCustomJk.js'

--- SrcJyCustomJk.js
+++ SrcJyCustomJk.js
@@ -74,6 +74,7 @@ let customparse = {
             var lists = [];
             let html = request("https://zhuiyingmao2.com/index.php/ajax/suggest?mid=1&wd="+name+"&limit=10" );
             let data = JSON.parse(html).list;
+            log(data.length)
             let cook = getCookie('https://zhuiyingmao2.com');
             data.forEach(item => {
                 let maoname = item.name;
@@ -89,6 +90,7 @@ let customparse = {
                     let maohtml = request(maourl, {
                         headers: headers
                     });
+                    log(maohtml)
                     let htmls = pdfa(maohtml, ".search-result-container&&a");
                     htmls.forEach(it => {
                         try {
