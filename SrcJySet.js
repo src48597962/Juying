77614190865aefe597cbc1789152593fdcd1b985
src48597962 <48597962@qq.com>
2023-10-10 21:33:28 +0800

    更新 'SrcJySet.js'

--- SrcJySet.js
+++ SrcJySet.js
@@ -1,5 +1,6 @@
 ////本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
 function SRCSet() {
+    log(aesDecode('Juying','K1zM/hocuUV5d8Q9T5IujP9pkulPKB77TVq7GjjEkXM='))
     addListener("onClose", $.toString(() => {
         clearMyVar('guanli');
         clearMyVar('guanlicz');
