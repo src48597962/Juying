48f07d3d6ef31029cd67b81444bb52c284575490
src48597962 <48597962@qq.com>
2023-12-15 09:22:42 +0800

    更新 'SrcJyAliDisk.js'

--- SrcJyAliDisk.js
+++ SrcJyAliDisk.js
@@ -495,7 +495,7 @@ function aliMyDisk(folder_id, isSearch, drive_id) {
         } else {
             try {
                 let postdata = { "drive_id": drive_id, "parent_file_id": folder_id, "limit": 200, "all": false, "url_expire_sec": 14400, "image_thumbnail_process": "image/resize,w_256/format,avif", "image_url_process": "image/resize,w_1920/format,avif", "video_thumbnail_process": "video/snapshot,t_1000,f_jpg,ar_auto,w_256", "fields": "*", "order_by": "updated_at", "order_direction": "DESC" };
-                headers['authorization'] = 'Bearer ' + userinfo.access_token;
+                headers['authorization'] = authorization;
                 headers['x-canary'] = "client=web,app=adrive,version=v4.1.1";
                 let getfiles = request('https://api.aliyundrive.com/adrive/v3/file/list', { headers: headers, body: postdata, method: 'POST' });
                 let myfilelist = JSON.parse(getfiles).items || [];
