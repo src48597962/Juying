1129ff04811de8dfaca873fdad19451da6df9e64
src48597962 <48597962@qq.com>
2024-05-26 19:51:45 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -853,16 +853,16 @@ function getYiData(jkdata) {
                             let typehtml = JSON.parse(gethtml);
                             typeclass = typehtml.class;
                         }
-                        /*
+                        
                         if(jkdata.categories){
                             for(var i=0;i<typeclass.length;i++){
-                                if(jkdata.categories.indexOf(typeclass[i].type_name)==-1 && typeclass[i].pid !=0){
+                                if(jkdata.categories.indexOf(typeclass[i].type_name)>-1 && typeclass[i].pid !=0){
                                     typeclass.splice(i,1);
                                     i = i -1;
                                 }
                             }
                         }
-                        */
+                        
                     }else {
                         log('api类型错误')
                     }
