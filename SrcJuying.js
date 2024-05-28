27a102a49de467bb944fb9ab2272401c308be505
src48597962 <48597962@qq.com>
2024-05-28 19:34:31 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -30,6 +30,7 @@ function search(name, sstype, jkdata) {
 // 软件搜索
 function sousuo() {
     log(MY_KEYWORD);
+    log(MY_URL.split('##'));
     let k = MY_URL.split('##')[1];
     let name,surl;
     if(k.includes('||')){
