a718bcb6f94e025eec9663b02eabc7e29c18f232
src48597962 <48597962@qq.com>
2024-05-30 08:35:12 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -467,7 +467,7 @@ function getYiData(jkdata) {
                 }*/
                 MY_URL = MY_URL.replace('or', '||').replace(/{{/g, '${').replace(/}}/g, '}');
                 eval(`MY_URL = \`${MY_URL}\`;`);
-                log(MY_URL.replace('fypage', MY_PAGE));
+                log(MY_URL);
                 //MY_URL = MY_URL.replace('fypage',MY_PAGE).replace('{{fl.year}}', year_id).replace('{{fl.area}}', area_id).replace('{{fl.by}}', sort_id).replace('{{fl.class}}', type_id).replace('{{fl.cateId}}', cate_id);
             }else if(api_type=="XBPQ"){
                 MY_URL = listurl.replace('/lang/{lang}','');
