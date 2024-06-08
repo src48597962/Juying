f818d9e4f7c3a167fc1fb030b68ab817ca50bcac
src48597962 <48597962@qq.com>
2024-06-09 07:57:06 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -1000,7 +1000,11 @@ function getErData(jkdata) {
                 if(extdata["线路二次截取"]){
                     arthtml = arthtml.split(extdata["线路二次截取"].split('&&')[0])[1].split(extdata["线路二次截取"].split('&&')[1])[0];
                 }
-                let artlist = arthtml.match(new RegExp(extdata["线路数组"].split('[')[0].replace('&&','((?:.|[\r\n])*?)'), 'g'));
+                extdata["线路数组"] = extdata["线路数组"].split('[')[0];
+                let artlist = arthtml.match(new RegExp(extdata["线路数组"].replace('&&','((?:.|[\r\n])*?)'), 'g'));
+                if(extdata["线路标题"].includes('+')){
+                    extdata["线路标题"] = extdata["线路标题"].split('+')[1];
+                }
                 for (let i = 0; i < artlist.length; i++) {
                     let arttitle = artlist[i].split(extdata["线路数组"].split('&&')[0])[1].split(extdata["线路数组"].split('&&')[1])[0].split(extdata["线路标题"].split('&&')[0])[1].split(extdata["线路标题"].split('&&')[1])[0];
                     tabs.push(arttitle.replace(/<\/?.+?\/?>/g,''));
