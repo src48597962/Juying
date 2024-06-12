2b2cdce65d6a9b5f5e5720dd91ee4771ca4023f4
src48597962 <48597962@qq.com>
2024-06-12 23:00:50 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -429,10 +429,10 @@ function getYiData(jkdata) {
                     pdfa(gethtml, extdata['分类列表数组规则']).forEach(it => {
                         let vodname = pdfh(it, extdata['分类片单标题']);
                         let vodid = pd(it, extdata['分类片单链接'], vodurlhead);
-                        let vodimg = (extdata["分类片单链接加前缀"] || "") + pdfh(it, extdata['分类片单图片']);
+                        let vodimg = pdfh(it, extdata['分类片单图片']);
                         let voddesc = pdfh(it, extdata['分类片单副标题']);
                         if (vodname && vodid) {
-                            vodimg = /^\//.test(vodimg) ? vodurlhead + vodimg : vodimg;
+                            vodimg = /^\//.test(vodimg) ? (extdata["分类片单链接加前缀"] || vodurlhead) + vodimg : vodimg;
                             vodimg = vodimg.includes('(') ? vodimg.match(/\(\'(.*?)\'\)/)[1] : vodimg;
                             let arr = { "vod_url": vodid, "vod_name": vodname, "vod_desc": voddesc, "vod_pic": vodimg };
                             vodlists.push(arr);
