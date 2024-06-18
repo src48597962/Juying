8d07bf97c5faf7e71e13a321f92161f2e4fbcdc8
src48597962 <48597962@qq.com>
2024-06-18 17:38:34 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -678,6 +678,7 @@ function getSsData(name, jkdata, page) {
                     }
                     json = { "list": xmllist };
                 } else {
+                    log(gethtml);
                     json = JSON.parse(gethtml);
                 }
             } else if (!/{|}/.test(gethtml) && gethtml != "") {
