6e8edba75581caf10be8ab339a10afb931177a00
src48597962 <48597962@qq.com>
2024-06-21 17:09:56 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -254,7 +254,7 @@ function dianboerji() {
     });
     //二级统一菜单
     require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJyMenu.js');
-    erjimenu(erdata.desc, sgroup).forEach(it=>{
+    erjimenu(erdata.desc, name, sgroup).forEach(it=>{
         d.push(it);
     })
 
