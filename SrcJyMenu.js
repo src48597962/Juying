9d01b9594adb06d3dee22ff0849529bc63bd9241
src48597962 <48597962@qq.com>
2024-06-23 13:11:55 +0800

    更新 'SrcJyMenu.js'

--- SrcJyMenu.js
+++ SrcJyMenu.js
@@ -1,6 +1,6 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
 //二级统一菜单
-function erjimenu(desc,name,sgroup) {
+function erjimenu(desc,name,group) {
     return [
         {
             title: "详情简介",
