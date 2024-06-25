a2a9df48825ab79d65399424ced6ffd5e74246b1
src48597962 <48597962@qq.com>
2024-06-25 20:46:29 +0800

    更新 'SrcLive.js'

--- SrcLive.js
+++ SrcLive.js
@@ -1,5 +1,5 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
-let datapath = "hiker://files/data2/"+MY_RULE.title+"/";
+let datapath = "hiker://files/data/"+MY_RULE.title+"/";
 putMyVar('JY直播数据路径', datapath);
 function Live() {
     addListener("onClose", $.toString(() => {
