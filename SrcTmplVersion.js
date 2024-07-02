266485dfd28fd65e5bfb67f1e4a7a8b285bf6c20
src48597962 <48597962@qq.com>
2024-07-02 14:04:17 +0800

    更新 'SrcTmplVersion.js'

--- SrcTmplVersion.js
+++ SrcTmplVersion.js
@@ -1,6 +1,7 @@
 var newVersion = {
     "SrcJuying":"0.1",
     "SrcJuyingdesc":{
-        "0.1":"重构聚影2.0，重新起航"
+        "0.1":"重构聚影2.0，重新起航，支持app类、cms、biubiu、xpath、XYQ、XBPQ、hipy接口 2024-07-01发布beta",
+        "0.2":"1.修复t4链接入参；2.修复hipy播放带header；3.修复订阅文件部份逻辑；"
     }
 };
