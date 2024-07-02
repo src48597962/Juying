6759e884b2cabb2e595b4417ccd23c5bc61ae4a6
src48597962 <48597962@qq.com>
2024-07-02 15:05:57 +0800

    更新 'SrcJyDrpy.js'

--- SrcJyDrpy.js
+++ SrcJyDrpy.js
@@ -28,87 +28,87 @@ function createOrGetEnvironment(id, ext) {
                 environments[id] = (function() {
                     // 创建一个闭包来隔离环境变量
                     //drpy运行环境相关
-                const localKey = "drpy";
-                globalThis.local = {
-                    set(rulekey, k, v) {
-                        storage0.setItem(localKey + "@" + rulekey + "@" + k, v);
-                    },
-                    get(rulekey, k, v) {
-                        return storage0.getItem(localKey + "@" + rulekey + "@" + k, "") || v;
-                    },
-                    delete(rulekey, k) {
-                        storage0.clearItem(localKey + "@" + rulekey + "@" + k);
-                    }
-                };
-                eval(getCryptoJS());
-                globalThis.CryptoJS = CryptoJS;
+                    const localKey = "drpy";
+                    globalThis.local = {
+                        set(rulekey, k, v) {
+                            storage0.setItem(localKey + "@" + rulekey + "@" + k, v);
+                        },
+                        get(rulekey, k, v) {
+                            return storage0.getItem(localKey + "@" + rulekey + "@" + k, "") || v;
+                        },
+                        delete(rulekey, k) {
+                            storage0.clearItem(localKey + "@" + rulekey + "@" + k);
+                        }
+                    };
+                    eval(getCryptoJS());
+                    globalThis.CryptoJS = CryptoJS;
 
-                let $request = request;
-                let $post = post;
-                globalThis.req = function (url, cobj) {
-                    try {
-                        let res = {};
-                        let obj = Object.assign({}, cobj);
-                        if (obj.data) {
-                            obj.body = obj.data;
-                            delete obj.data;
-                        }
-
-                        if (obj.hasOwnProperty("redirect")) obj.redirect = !!obj.redirect;
-                        if (obj.buffer === 2) {
-                            obj.toHex = true;
-                        }
-                        obj.headers = Object.assign({
-                            Cookie: "#noCookie#"
-                        }, obj.headers);
-                        if (url === "https://api.nn.ci/ocr/b64/text" && obj.headers) {
-                            obj.headers["Content-Type"] = "text/plain";
-                        }
-
-                        if (url.startsWith("file://") && (url.includes("?type=") || url.includes("?params="))) {
-                            url = url.slice(0, url.lastIndexOf("?"));
-                        }
-                        for (let key in obj.headers) {
-                            if (typeof obj.headers[key] !== "string") {
-                                obj.headers[key] = String(obj.headers[key]);
+                    let $request = request;
+                    let $post = post;
+                    globalThis.req = function (url, cobj) {
+                        try {
+                            let res = {};
+                            let obj = Object.assign({}, cobj);
+                            if (obj.data) {
+                                obj.body = obj.data;
+                                delete obj.data;
                             }
-                        }
-                        let r = "";
-                        r = $request(url, obj);
-                        if (obj.withHeaders) {
-                            r = JSON.parse(r);
-                            res.content = r.body;
-                            res.headers = {};
-                            for (let [k, v] of Object.entries(r.headers || {})) {
-                                res.headers[k] = v[0];
-                            }
-                        } else {
-                            res.content = r;
-                        }
-                        if (obj.buffer === 2) {
-                            const CryptoUtil = $.require("hiker://assets/crypto-java.js");
-                            res.content = CryptoUtil.Data.parseHex(res.content).toBase64(_base64.NO_WRAP);
-                        }
-                        return res;
-                    } catch (e) {
-                        log("Error" + e.toString());
-                    }
-                }
-                pdfa = _pdfa;
-                pd = _pd;
-                pdfh = _pdfh;
-                String.prototype.replaceAll = function (search, replacement) {
-                    return this.split(search).join(replacement);
-                };
-                let $toString = Function.prototype.toString;
-                Function.prototype.toString = function () {
-                    return $toString.apply(this).trim();
-                };
-                let drpy2 = $.require(module.modulePath.slice(0, module.modulePath.lastIndexOf("/")) + '/drpy/drpy2.js');
-                return drpy2.DRPY();
-            })();
-            environments[id].init(ext);
 
+                            if (obj.hasOwnProperty("redirect")) obj.redirect = !!obj.redirect;
+                            if (obj.buffer === 2) {
+                                obj.toHex = true;
+                            }
+                            obj.headers = Object.assign({
+                                Cookie: "#noCookie#"
+                            }, obj.headers);
+                            if (url === "https://api.nn.ci/ocr/b64/text" && obj.headers) {
+                                obj.headers["Content-Type"] = "text/plain";
+                            }
+
+                            if (url.startsWith("file://") && (url.includes("?type=") || url.includes("?params="))) {
+                                url = url.slice(0, url.lastIndexOf("?"));
+                            }
+                            for (let key in obj.headers) {
+                                if (typeof obj.headers[key] !== "string") {
+                                    obj.headers[key] = String(obj.headers[key]);
+                                }
+                            }
+                            let r = "";
+                            r = $request(url, obj);
+                            if (obj.withHeaders) {
+                                r = JSON.parse(r);
+                                res.content = r.body;
+                                res.headers = {};
+                                for (let [k, v] of Object.entries(r.headers || {})) {
+                                    res.headers[k] = v[0];
+                                }
+                            } else {
+                                res.content = r;
+                            }
+                            if (obj.buffer === 2) {
+                                const CryptoUtil = $.require("hiker://assets/crypto-java.js");
+                                res.content = CryptoUtil.Data.parseHex(res.content).toBase64(_base64.NO_WRAP);
+                            }
+                            return res;
+                        } catch (e) {
+                            log("Error" + e.toString());
+                        }
+                    }
+                    pdfa = _pdfa;
+                    pd = _pd;
+                    pdfh = _pdfh;
+                    String.prototype.replaceAll = function (search, replacement) {
+                        return this.split(search).join(replacement);
+                    };
+                    let $toString = Function.prototype.toString;
+                    Function.prototype.toString = function () {
+                        return $toString.apply(this).trim();
+                    };
+                    let drpy2 = $.require(module.modulePath.slice(0, module.modulePath.lastIndexOf("/")) + '/drpy/drpy2.js');
+                    return drpy2.DRPY();
+                })();
+                environments[id].init(ext);
+                log($.type(environments[id]));
                 resolve(environments[id]);
             } catch (error) {
                 reject(error);
