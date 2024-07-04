fb7f9219853c38730521b89e85871079b2b1d1f8
src48597962 <48597962@qq.com>
2024-07-04 11:40:07 +0800

    更新 'SrcTmplVersion.js'

--- SrcTmplVersion.js
+++ SrcTmplVersion.js
@@ -1,7 +1,7 @@
 var newVersion = {
     "SrcJuying":"0.1",
     "SrcJuyingdesc":{
-        "0.1":"重构聚影2.0，重新起航，支持app类、cms、biubiu、xpath、XYQ、XBPQ、hipy接口 2024-07-01发布beta",
-        "0.2":"1.修复t4链接入参；2.修复hipy播放带header；3.修复订阅文件部份逻辑；"
+        "0.1":"2024-07-01；；1.重构聚影2.0，重新起航；2.支持app类、cms、biubiu、xpath、XYQ、XBPQ、hipy接口；3.支持本地接口或订阅文件；4.保留云盘、直播、alist等原聚影融合模块",
+        "0.2":"1.修复t4链接入参；2.修复hipy播放带header；3.修复订阅文件部份逻辑，支持本地t3；4.接口管理模块存在的bug修复；5.t3环境引擎采用L佬的代码；"
     }
 };
