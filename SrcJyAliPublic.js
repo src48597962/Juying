c89bf5fb9cdb698e6c3043f633bc45c5aabfb704
src48597962 <48597962@qq.com>
2024-07-12 21:59:11 +0800

    更新 'SrcJyAliPublic.js'

--- SrcJyAliPublic.js
+++ SrcJyAliPublic.js
@@ -1,6 +1,7 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
+require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJyPublic.js');
 //Ali公用文件
-let alicfgfile = "hiker://files/rules/Src/Juying2/aliconfig.json";
+let alicfgfile = rulepath + "aliconfig.json";
 let aliconfig = {};
 if (fetch(alicfgfile)) {
     try {
