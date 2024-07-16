3b5247b863751fcac8b04fa5b8a21a507006855f
src48597962 <48597962@qq.com>
2024-07-16 12:30:14 +0800

    更新 'SrcJySet.js'

--- SrcJySet.js
+++ SrcJySet.js
@@ -49,7 +49,7 @@ function SRCSet() {
                 jiexi();
             }
         }, guanliType),
-        img: getIcon("接口-增加.svg"),//"https://hikerfans.com/tubiao/more/25.png",
+        img: getIcon("点播-增加.svg"),//"https://hikerfans.com/tubiao/more/25.png",
         col_type: "icon_small_4"
     });
     d.push({
@@ -81,7 +81,7 @@ function SRCSet() {
                 })
             }
         }),
-        img: getIcon("接口-操作.svg"),//"https://hikerfans.com/tubiao/more/290.png",
+        img: getIcon("点播-操作.svg"),//"https://hikerfans.com/tubiao/more/290.png",
         col_type: "icon_small_4"
     });
     d.push({
@@ -96,7 +96,7 @@ function SRCSet() {
             require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJySet.js');
             return JYimport(input);
         }),
-        img: getIcon("接口-导入.svg"),//"https://hikerfans.com/tubiao/more/43.png",
+        img: getIcon("点播-导入.svg"),//"https://hikerfans.com/tubiao/more/43.png",
         col_type: "icon_small_4"
     });
     let pastes = getPastes();
@@ -121,7 +121,7 @@ function SRCSet() {
             require(config.依赖.match(/http(s)?:\/\/.*\//)[0] + 'SrcJySet.js');
             return JYshare(lx, input);
         }, guanliType),
-        img: getIcon("接口-分享.svg"),//"https://hikerfans.com/tubiao/more/3.png",
+        img: getIcon("点播-分享.svg"),//"https://hikerfans.com/tubiao/more/3.png",
         col_type: "icon_small_4",
         extra: {
             longClick: [{
@@ -2390,7 +2390,7 @@ function yundiskjiekou() {
         url: $('hiker://empty#noRecordHistory##noHistory#').rule((filepath,yundiskapi) => {
             yundiskapi(filepath);
         },filepath,yundiskapi),
-        img: getIcon("接口-增加.svg"),//"https://hikerfans.com/tubiao/more/25.png",
+        img: getIcon("云盘-增加.svg"),//"https://hikerfans.com/tubiao/more/25.png",
         col_type: "icon_small_3"
     });
     d.push({
@@ -2417,7 +2417,7 @@ function yundiskjiekou() {
                 return "toast://聚影✓：口令有误";
             }
         }),
-        img: getIcon("接口-导入.svg"),//"https://hikerfans.com/tubiao/more/43.png",
+        img: getIcon("云盘-导入.svg"),//"https://hikerfans.com/tubiao/more/43.png",
         col_type: "icon_small_3"
     });
     d.push({
@@ -2432,7 +2432,7 @@ function yundiskjiekou() {
                 return "toast://分享失败，剪粘板或网络异常";
             }
         }, datalist),
-        img: getIcon("接口-分享.svg"),//"https://hikerfans.com/tubiao/more/3.png",
+        img: getIcon("云盘-分享.svg"),//"https://hikerfans.com/tubiao/more/3.png",
         col_type: "icon_small_3",
         extra: {
             longClick: [{
