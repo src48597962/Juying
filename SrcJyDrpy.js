3bee18910f7d8a0d4a4d5ff88ada25cdf47c95da
src48597962 <48597962@qq.com>
2024-07-18 22:25:11 +0800

    更新 'SrcJyDrpy.js'

--- SrcJyDrpy.js
+++ SrcJyDrpy.js
@@ -34,6 +34,7 @@ function createDrpy(key) {
         };
         eval(getCryptoJS());
         globalThis.CryptoJS = CryptoJS;
+        log(GMkey);
         globalThis.getProxy = function () {
             return  globalMap0.getMyVar("proxyUrl", "http://127.0.0.1:52020/proxy") + "?do=js";
         }
