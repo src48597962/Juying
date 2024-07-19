0b21fde8dd33d35d6eac16893dd586770b2c8c41
src48597962 <48597962@qq.com>
2024-07-19 11:16:52 +0800

    更新 'SrcJyPublic.js'

--- SrcJyPublic.js
+++ SrcJyPublic.js
@@ -660,6 +660,39 @@ function getDrpyExt(jkdata) {
     }
     return '';
 }
+// 启动代理
+function startProxy(key, ext) {
+    let proxyUrl = startProxyServer($.toString((api_name, jk_api_ext, codepath, title) => {
+        log("进来了");
+        
+        let {GM} = $.require("http://hiker.nokia.press/hikerule/rulelist.json?id=6916&auth=1d35e8f0-22e8-5270-a9d1-826f53f177ad");
+        GM.setSelfKey(title);
+        let drpy = GM.defineModule("SrcJuDrpy", codepath + "SrcJyDrpy.js").get(api_name, jk_api_ext);
+
+        let params = {};
+        for (let key in MY_PARAMS) {
+            params[key] = String(MY_PARAMS[key][0]);
+        }
+        
+        let result = drpy.proxy(params);
+        let [code, media_type, data, headers, isReturnBytes] = result;
+        headers = Object.assign({}, {
+            'Content-Type': media_type,
+        }, headers);
+        if(typeof data==="string"&&data.startsWith("data:")&&data.includes("base64,")){
+            data = data.split("base64,")[1];
+            const CryptoUtil = $.require("hiker://assets/crypto-java.js");
+            data = CryptoUtil.Data.parseBase64(data).toBytes();
+        }
+        return {
+            statusCode: code,
+            body: data,
+            headers: headers,
+        };
+        
+    },key, ext, codepath, MY_RULE._title||MY_RULE.title));
+    return proxyUrl;
+}
 
 let gmParams = {
     datapath:datapath,
