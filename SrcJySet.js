65dd2dc0556dd3fca25da9e046d4546191dfebb7
src48597962 <48597962@qq.com>
2024-07-20 10:38:07 +0800

    更新 'SrcJySet.js'

--- SrcJySet.js
+++ SrcJySet.js
@@ -2700,7 +2700,7 @@ function importConfirm(input) {
         let isnew = ndatalist.some(v=>v.url==it.url);
         d.push({
             title: it.name + "-" + (it.group||it.type) + "  [" + (isnew?"新增加":"已存在") + "]",
-            url: $(["覆盖导入"], 2).select((lx, data) => {
+            url: $(["覆盖导入"], 1).select((lx, data) => {
                 data = JSON.parse(base64Decode(data));
                 if (input == "覆盖导入") {
                     return $("将覆盖本地，确认？").confirm((lx,data)=>{
