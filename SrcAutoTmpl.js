6528bd57cda2666720270f25e52b8d42eaf9e88d
src48597962 <48597962@qq.com>
2024-07-24 16:20:29 +0800

    更新 'SrcAutoTmpl.js'

--- SrcAutoTmpl.js
+++ SrcAutoTmpl.js
@@ -1,78 +1,78 @@
 //本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
-function autoerji(url,html){
-    let data = {};
-    if(!/http/.test(url)){return data;}
-    let html = html||request(url, {headers: {'User-Agent': PC_UA }, timeout: 5000});
-    let urldomian = getHome(url);
+function autoerji(url, html) {
+	let data = {};
+	if (!/http/.test(url)) { return data; }
+	let html = html || request(url, { headers: { 'User-Agent': PC_UA }, timeout: 5000 });
+	let urldomian = getHome(url);
 	log(urldomian);
-	
-    let urltmpl = JSON.parse(getMyVar('Tmpl-'+urldomian,'{}'));
-    let tmplidex = erjiTmpl.findIndex(it=>it.id===urltmpl.id);
-    if(tmplidex>-1) {
-        let tmpl = erjiTmpl.splice(tmplidex, 1)[0];
-        erjiTmpl.unshift(tmpl);
-    }
+
+	let urltmpl = JSON.parse(getMyVar('Tmpl-' + urldomian, '{}'));
+	let tmplidex = erjiTmpl.findIndex(it => it.id === urltmpl.id);
+	if (tmplidex > -1) {
+		let tmpl = erjiTmpl.splice(tmplidex, 1)[0];
+		erjiTmpl.unshift(tmpl);
+	}
 	let detail = {};
 	//线程
-	let task = function(obj) {
-        //log('【'+obj.id+'】');
+	let task = function (obj) {
+		//log('【'+obj.id+'】');
 		let arts = _pdfa(html, obj.tabs);
 		let tabs = [];
 		arts.forEach(item => {
-			let name = pdfh(item, obj.tab_text?obj.tab_text:'h3||a||span||body&&Text');
-			if(name&&!/更多精品/.test(name)){
+			let name = pdfh(item, obj.tab_text ? obj.tab_text : 'h3||a||span||body&&Text');
+			if (name && !/更多精品/.test(name)) {
 				tabs.push(name);
 			}
 		});
-		
-		let conts = _pdfa(html,'body&&'+obj.lists.split(';')[0]);//全线路影片列表
+
+		let conts = _pdfa(html, 'body&&' + obj.lists.split(';')[0]);//全线路影片列表
 		let lists = [];
 		let key = obj.lists.split(';')[1];
-		conts.forEach(item=>{
+		conts.forEach(item => {
 			let cont = _pdfa(item, key);//单线路影片列表
 			let list = [];
 			for (let j = 0; j < cont.length; j++) {
-				let listname = _pdfh(cont[j],"a&&Text");
-				let listurl = _pd(cont[j], obj.tab_id||'a&&href', urldomian);
-				if(listname&&listurl){
-					list.push(listname+"$"+listurl);
+				let listname = _pdfh(cont[j], "a&&Text");
+				let listurl = _pd(cont[j], obj.tab_id || 'a&&href', urldomian);
+				if (listname && listurl) {
+					list.push(listname + "$" + listurl);
 				}
 			}
-			if(list.length>0){
+			if (list.length > 0) {
 				lists.push(list);
 			}
 		})
-		let details1,pic,desc;
-		try{
+		let details1, pic, desc;
+		try {
 			let details = obj.desc.split(';');
 			details1 = "";
-			for(let j=0;j<details.length;j++){
+			for (let j = 0; j < details.length; j++) {
 				details1 = details1.concat(_pdfh(html, details[j]));
 			}
-			if(details1&&!detail.details1){detail.details1 = details1;}
-		}catch(e){}
-		
-		try{
-			pic = _pdfh(html, obj.img).replace(/http.*\/tu\.php\?tu=|\/img\.php\?url=| |\/tu\.php\?tu=/g,'');
-			if(!/^http/.test(pic)&&pic){
+			if (details1 && !detail.details1) { detail.details1 = details1; }
+		} catch (e) { }
+
+		try {
+			pic = _pdfh(html, obj.img).replace(/http.*\/tu\.php\?tu=|\/img\.php\?url=| |\/tu\.php\?tu=/g, '');
+			if (!/^http/.test(pic) && pic) {
 				pic = urldomian + pic;
 			}
-			if(pic&&!detail.pic){detail.pic = pic;}
-		}catch(e){}
-		try{
-			desc = obj.content?_pdfh(html,obj.content):"";
-			if(desc&&!detail.desc){detail.desc = desc;}
-		}catch(e){}
-		if(obj.id==35){
+			if (pic && !detail.pic) { detail.pic = pic; }
+		} catch (e) { }
+		try {
+			desc = obj.content ? _pdfh(html, obj.content) : "";
+			if (desc && !detail.desc) { detail.desc = desc; }
+		} catch (e) { }
+		if (obj.id == 35) {
 			log(obj.id);
 			log(tabs);
 			log(lists);
 		}
-        return {details1:details1,pic:pic,desc:desc,tabs:tabs,lists:lists};
-    };
+		return { details1: details1, pic: pic, desc: desc, tabs: tabs, lists: lists };
+	};
 
 	let setid = 0;
-    let Tmpls = erjiTmpl.map((item)=>{
+	let Tmpls = erjiTmpl.map((item) => {
 		return {
 			func: task,
 			param: item,
@@ -81,8 +81,8 @@ function autoerji(url,html){
 	});
 	let t = {};
 	be(Tmpls, {
-		func: function(obj, id, error, taskResult) {
-			if (taskResult.tabs.length>0&&taskResult.lists.length>0) {
+		func: function (obj, id, error, taskResult) {
+			if (taskResult.tabs.length > 0 && taskResult.lists.length > 0) {
 				setid = id;
 				data = taskResult;
 				o = obj;
@@ -93,32 +93,32 @@ function autoerji(url,html){
 			o: t,
 		}
 	});
-	if(setid>0&&data.tabs.length>0&&data.lists.length>0){
-		putMyVar('Tmpl-'+urldomian, JSON.stringify(t));
-		if(data.tabs.length>data.lists.length){
-			data.tabs.splice(data.lists.length-1, data.tabs.length-data.lists.length);
+	if (setid > 0 && data.tabs.length > 0 && data.lists.length > 0) {
+		putMyVar('Tmpl-' + urldomian, JSON.stringify(t));
+		if (data.tabs.length > data.lists.length) {
+			data.tabs.splice(data.lists.length - 1, data.tabs.length - data.lists.length);
 		}
 		data.details2 = "数据来源：模板匹配";
-		data.details1 = data.details1||detail.details1||"";
-		data.pic = data.pic||detail.pic||"";
-		data.desc = data.desc||detail.desc||"";
-	}else{
+		data.details1 = data.details1 || detail.details1 || "";
+		data.pic = data.pic || detail.pic || "";
+		data.desc = data.desc || detail.desc || "";
+	} else {
 		data = {};
 	}
-	if(data.lists){
+	if (data.lists) {
 		return data;
-	}else{
-		return aierji(html,url,detail);
+	} else {
+		return aierji(html, url, detail);
 	}
 }
 //AI二级
-function aierji(html,url,detail){
+function aierji(html, url, detail) {
 	var d = [];
 	let alist = pdfa(html, "body&&a");
 	let arr = alist.map(it => {
 		return {
 			//html: it,
-			text: pdfh(it, "a&&Text").replace(/new|最新|新/g,''),
+			text: pdfh(it, "a&&Text").replace(/new|最新|新/g, ''),
 			title: pdfh(it, "a&&title"),
 			href: pd(it, "a&&href", url)
 		}
@@ -209,7 +209,7 @@ function aierji(html,url,detail){
 	for (let i = 0; i < arr.length; i++) {
 		let it = arr[i];
 		let t = it.text;
-		if (!it.href || /voddetail|s\/guoyu|vodshow|#playlist/.test(it.href) || (it.href.split('/').length<5)) {
+		if (!it.href || /voddetail|s\/guoyu|vodshow|#playlist/.test(it.href) || (it.href.split('/').length < 5)) {
 			continue;
 		}
 		let pre = i == 0 ? null : arr[i - 1];
@@ -228,7 +228,7 @@ function aierji(html,url,detail){
 		let lists = [];
 		let d2 = [];
 		for (let i = 0; i < d.length; i++) {
-			d2.push(d[i].title+'$'+d[i].url);
+			d2.push(d[i].title + '$' + d[i].url);
 			if (i < d.length - 1) {
 				let it = d[i];
 				let t1 = parseInt(clearText(it.title));
@@ -236,23 +236,23 @@ function aierji(html,url,detail){
 				let t2 = parseInt(clearText(next.title));
 				if (t2 - t1 > 1 || t1 - t2 > 1) {
 					lists.push(d2);
-					let s = tabs.length+1;
-					tabs.push("线路"+s);
+					let s = tabs.length + 1;
+					tabs.push("线路" + s);
 					d2 = [];
 				}
-			}else{
+			} else {
 				lists.push(d2);
 			}
 		}
-		if(lists.length==0){tabs = [];}
-		return {details2: "数据来源：AI识片",details1:detail.details1||"",pic:detail.pic,desc:detail.desc,tabs:tabs,lists:lists};
+		if (lists.length == 0) { tabs = []; }
+		return { details2: "数据来源：AI识片", details1: detail.details1 || "", pic: detail.pic, desc: detail.desc, tabs: tabs, lists: lists };
 	}
 	return {};
 }
 
 let erjiTmpl = [
 	{
-    	"id": 1,
+		"id": 1,
 		"img": ".lazyload&&data-original",
 		"desc": ".data&&Text;.data,6&&Text",
 		"content": "p.detail&&Text.js:input.replace(\"简介：\",\"\").replace(\"详情\",\"\")",
@@ -260,14 +260,14 @@ let erjiTmpl = [
 		"lists": ".stui-content__playlist;body&&li"
 	},
 	{
-        "id": 2,
+		"id": 2,
 		"img": ".lazyload&&data-original",
 		"desc": "span.sketch.content&&Text",
 		"tabs": ".nav-tabs&&li",
 		"lists": ".myui-content__list;body&&li"
 	},
 	{
-        "id": 3,
+		"id": 3,
 		"img": ".lazyload&&data-original",
 		"desc": ".module-info-item,1&&Text;.module-info-item,2&&Text;.module-info-item,3&&Text",
 		"content": ".module-info-introduction-content&&Text",
@@ -275,7 +275,7 @@ let erjiTmpl = [
 		"lists": "body&&.module-play-list;body&&a"
 	},
 	{
-        "id": 4,
+		"id": 4,
 		"img": ".myui-content__thumb&&a&&img&&data-original",
 		"desc": ".data,2&&Text;.data,3&&Text",
 		"content": "#desc&&span.data&&Text",
@@ -283,7 +283,7 @@ let erjiTmpl = [
 		"lists": ".tab-content&&#id&&li"
 	},
 	{
-        "id": 5,
+		"id": 5,
 		"img": ".lazyload&&data-src",
 		"desc": ".tag-link&&Text",
 		"content": ".video-info-content&&Text",
@@ -291,7 +291,7 @@ let erjiTmpl = [
 		"lists": "body&&.scroll-box-y;body&&a"
 	},
 	{
-        "id": 6,
+		"id": 6,
 		"img": ".lazyload&&data-original",
 		"desc": "p.data,-1&&Text;p.data,-2&&Text",
 		"content": "body&&.stui-pannel_bd:not(:has(a)):has(.col-pd)&&Text",
@@ -299,7 +299,7 @@ let erjiTmpl = [
 		"lists": "body&&.stui-content__playlist;body&&li"
 	},
 	{
-        "id": 7,
+		"id": 7,
 		"img": ".lazyload&&data-original",
 		"desc": "p.data&&Text;.stui-content__detail&&p,-2&&Text",
 		"content": ".detail&&Text",
@@ -307,7 +307,7 @@ let erjiTmpl = [
 		"lists": ".stui-content__playlist;body&&li"
 	},
 	{
-        "id": 8,
+		"id": 8,
 		"img": ".lazyload&&data-original",
 		"desc": "p.data&&Text;.stui-content__detail&&p,-2&&Text",
 		"content": ".desc&&Text",
@@ -316,7 +316,7 @@ let erjiTmpl = [
 		"lists": ".stui-content__playlist;body&&li"
 	},
 	{
-        "id": 9,
+		"id": 9,
 		"img": ".fed-lazy&&data-original",
 		"desc": ".fed-list-remarks&&Text;.fed-deta-info&&li,-2&&Text;.fed-deta-info&&li,-3&&Text",
 		"content": ".fed-tabs-info&&p&&Text",
@@ -324,7 +324,7 @@ let erjiTmpl = [
 		"lists": ".fed-play-item;body&&ul,1&&li"
 	},
 	{
-        "id": 10,
+		"id": 10,
 		"img": ".hl-lazy&&data-original",
 		"desc": ".hl-full-box&&li,1&&Text",
 		"content": ".hl-full-box&&li,-1&&Text",
@@ -332,7 +332,7 @@ let erjiTmpl = [
 		"lists": "body&&.hl-plays-list;body&&li"
 	},
 	{
-        "id": 11,
+		"id": 11,
 		"img": ".lazy&&data-original",
 		"desc": ".vodTag&&Text",
 		"content": ".ecshow&&Text",
@@ -340,7 +340,7 @@ let erjiTmpl = [
 		"lists": "body&&.content_playlist;body&&a"
 	},
 	{
-        "id": 12,
+		"id": 12,
 		"img": ".lazyload&&data-original",
 		"desc": ".content_min&&li,1&&Text",
 		"content": ".content_min&&li,-1&&Text",
@@ -348,7 +348,7 @@ let erjiTmpl = [
 		"lists": "body&&.content_playlist;body&&li"
 	},
 	{
-        "id": 13,
+		"id": 13,
 		"img": ".lazy&&data-original",
 		"desc": ".mv-showr&&p,2&&Text;.mv-showr&&p,3&&Text",
 		"content": ".des&&Text",
@@ -356,14 +356,14 @@ let erjiTmpl = [
 		"lists": ".layui-tab-content,1&&.layui-tab-item:has(a);body&&a"
 	},
 	{
-        "id": 14,
+		"id": 14,
 		"img": ".y-part-2by3&&data-original",
 		"desc": ".y-part-rows&&li&&Text&&Text;.y-part-rows&&li,1&&Text",
 		"tabs": ".nav-tabs&&li",
 		"lists": ".episodes-list;body&&li"
 	},
 	{
-        "id": 15,
+		"id": 15,
 		"img": ".fed-part-2by3&&data-original",
 		"desc": ".fed-deta-content&&.fed-part-rows&&li&&Text",
 		"content": ".fed-conv-text&&Text",
@@ -371,7 +371,7 @@ let erjiTmpl = [
 		"lists": ".fed-tabs-btm;body&&li"
 	},
 	{
-        "id": 16,
+		"id": 16,
 		"img": ".detail-pic&&img&&src",
 		"desc": ".addtime&&Text",
 		"content": ".info&&Text",
@@ -379,7 +379,7 @@ let erjiTmpl = [
 		"lists": "body&&.video_list;body&&a"
 	},
 	{
-        "id": 17,
+		"id": 17,
 		"img": ".cover&&img&&src",
 		"desc": ".v_desc&&Text;#intro&&p,0&&Text;#intro&&p,2&&Text;#intro&&p,1&&Text;#intro&&p,3&&Text",
 		"content": "#intro&&p,-1&&Text;",
@@ -387,7 +387,7 @@ let erjiTmpl = [
 		"lists": "#play_list&&.play_list;body&&li"
 	},
 	{
-        "id": 18,
+		"id": 18,
 		"img": ".poster&&img&&src",
 		"desc": ".detail_imform_kv,0&&Text;.detail_imform_kv,2&&Text;.detail_imform_kv,5&&Text",
 		"content": ".detail_imform_desc_pre&&p&&Text",
@@ -395,7 +395,7 @@ let erjiTmpl = [
 		"lists": "#main0&&.movurl;body&&li"
 	},
 	{
-        "id": 19,
+		"id": 19,
 		"img": ".lazy&&data-original",
 		"desc": ".info-txt&&p,2&&Text;.info-txt&&p,3&&Text;",
 		"content": ".brief&&Text",
@@ -403,7 +403,7 @@ let erjiTmpl = [
 		"lists": ".playerlist;body&&ul&&a"
 	},
 	{
-        "id": 20,
+		"id": 20,
 		"img": ".poster&&a&&style",
 		"desc": ".play-tag&&Text",
 		"content": ".info-wrap,-1&&Text",
@@ -411,7 +411,7 @@ let erjiTmpl = [
 		"lists": "#playsx;body&&li"
 	},
 	{
-        "id": 21,
+		"id": 21,
 		"img": "",
 		"desc": "body&&.data&&Text",
 		"content": ".stui-player__detail&&p,-1&&Text",
@@ -419,7 +419,7 @@ let erjiTmpl = [
 		"lists": ".stui-play__list;body&&li"
 	},
 	{
-        "id": 22,
+		"id": 22,
 		"img": ".pic&&img&&src",
 		"desc": "body&&dd,0&&Text;info&&dd,1&&Text;.info&&dd,2&&Text",
 		"content": ".desdd&&Text",
@@ -427,7 +427,7 @@ let erjiTmpl = [
 		"lists": "body&&.plist;body&&a"
 	},
 	{
-        "id": 23,
+		"id": 23,
 		"img": ".pic&&img&&src",
 		"desc": "body&&dd,0&&Text;info&&dd,1&&Text;.info&&dd,2&&Text",
 		"content": ".desdd&&Text",
@@ -435,7 +435,7 @@ let erjiTmpl = [
 		"lists": "body&&.plist;body&&a"
 	},
 	{
-        "id": 24,
+		"id": 24,
 		"img": ".dyimg&&img&&src",
 		"desc": ".data,0&&Text",
 		"content": "#desc&&.stui-content__desc&&Text",
@@ -443,7 +443,7 @@ let erjiTmpl = [
 		"lists": ".stui-content__playlist;body&&li"
 	},
 	{
-        "id": 25,
+		"id": 25,
 		"img": ".lazyload&&data-original",
 		"desc": ".fed-deta-content&&.fed-part-rows&&Text",
 		"content": ".fed-tabs-info&&p&&Text",
@@ -451,7 +451,7 @@ let erjiTmpl = [
 		"lists": ".fed-play-item;body&&ul,1&&li"
 	},
 	{
-        "id": 26,
+		"id": 26,
 		"img": ".lazyload&&data-original",
 		"desc": ".play-ail&&p,-2&&Text",
 		"content": ".detail-intro-txt&&Text",
@@ -459,7 +459,7 @@ let erjiTmpl = [
 		"lists": ".playlist&&ul;body&&li"
 	},
 	{
-        "id": 27,
+		"id": 27,
 		"img": ".v-pic&&img&&src",
 		"desc": ".txt_list&&li,2&&Text;.txt_list&&li,3&&Text;.txt_list&&li,4&&Text",
 		"content": ".infor_intro&&Text",
@@ -467,7 +467,7 @@ let erjiTmpl = [
 		"lists": "body&&.play_num_list;body&&li"
 	},
 	{
-        "id": 28,
+		"id": 28,
 		"img": ".dyimg&&img&&src",
 		"desc": ".moviedteail_list&&Text",
 		"content": "body&&.yp_context&&Text",
@@ -475,7 +475,7 @@ let erjiTmpl = [
 		"lists": ".paly_list_btn;body&&a"
 	},
 	{
-        "id": 29,
+		"id": 29,
 		"img": ".lazyload&&data-original",
 		"desc": ".myui-content__detail&&.data&&Text",
 		"content": "#rating&&Text",
@@ -484,7 +484,7 @@ let erjiTmpl = [
 		"lists": "#playlist1&&li;body&&a"
 	},
 	{
-        "id": 30,
+		"id": 30,
 		"img": ".m_background&&style",
 		"desc": ".v_info_box&&p&&Text",
 		"content": ".p_txt&&Text",
@@ -492,7 +492,7 @@ let erjiTmpl = [
 		"lists": "#play_link&&li;body&&a"
 	},
 	{
-        "id": 31,
+		"id": 31,
 		"img": ".lazy&&src",
 		"desc": ".detail_top&&li,2&&Text;.detail_top&&li,,3&&Text;.detail_top&&li,4&&Text",
 		"content": ".li_intro&&Text",
@@ -500,7 +500,7 @@ let erjiTmpl = [
 		"lists": "body&&.player_list;body&&li"
 	},
 	{
-        "id": 32,
+		"id": 32,
 		"img": "#book-cont&&img&&src",
 		"desc": "#book-cont&&.r&&Text",
 		"content": "#wrap&&Text",
@@ -508,7 +508,7 @@ let erjiTmpl = [
 		"lists": "#zhankai&&.arconix-toggle-content;body&&li"
 	},
 	{
-        "id": 33,
+		"id": 33,
 		"img": "#imglazy&&src",
 		"desc": ".info,0&&Text;.info,1&&Text;.info,2&&Text;.starring&&Text",
 		"content": ".animeplot&&Text",
@@ -516,7 +516,7 @@ let erjiTmpl = [
 		"lists": ".playlist;body&&.list-title"
 	},
 	{
-        "id": 34,
+		"id": 34,
 		"img": ".lazyload&&data-original||src",
 		"desc": ".module-info-item,3&&Text;.module-info-item,1&&Text;.module-info-item,4&&Text",
 		"content": ".module-info-item,5||.module-info-item&&Text",
@@ -524,7 +524,7 @@ let erjiTmpl = [
 		"lists": ".playlist;body&&.list-title"
 	},
 	{
-        "id": 35,
+		"id": 35,
 		"img": ".lazyload&&data-original||src",
 		"desc": ".data&&Text;.data,1&&Text;.data,6&&Text",
 		"content": ".detail-content&&&&Text",
