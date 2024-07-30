fa92e2068fa6cff9156365d5524c9926c9c5a5c4
src48597962 <48597962@qq.com>
2024-07-30 16:11:14 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -762,10 +762,12 @@ function getSsData(name, jkdata, page) {
                 json = JSON.parse(getHtml(jkdata.url+"&wd="+name+"&extend="+jkdata.ext+"&quick=false", headers));
             }else if(api_type=="hipy_t3"){
                 let drpy = GM.defineModule("SrcJuDrpy", config.依赖.match(/http(s)?:\/\/.*\//)[0] + "SrcJyDrpy.js").get(jkdata);
+                /*
                 try{
-                    log(drpy.search(name, 0, page));
                     json = JSON.parse(drpy.search(name, 0, page));
                 }catch(e){}
+                */
+                log(drpy.search(name, 1, 1));
                 noerji = drpy.getRule("二级")=="*"?1:0;
             }else{
                 gethtml = getHtmlCode(ssurl, headers);
