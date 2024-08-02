65350fd2d5cced9fb26d65fc74216a34ea08044e
src48597962 <48597962@qq.com>
2024-08-02 11:10:34 +0800

    更新 'SrcJyPublic.js'

--- SrcJyPublic.js
+++ SrcJyPublic.js
@@ -852,5 +852,6 @@ let gmParams = {
     getContnet: getContnet
 }
 globalMap0.putMyVar('gmParams', gmParams);
+let {GM} = $.require("http://hiker.nokia.press/hikerule/rulelist.json?id=6916&auth=1d35e8f0-22e8-5270-a9d1-826f53f177ad");
 GM.put('gmParams', gmParams);
 
