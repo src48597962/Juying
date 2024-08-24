be00b99e99eff31a743951ca9771812fb2e9462e
src48597962 <48597962@qq.com>
2024-08-24 13:10:49 +0800

    更新 'SrcParseS.js'

--- SrcParseS.js
+++ SrcParseS.js
@@ -67,6 +67,7 @@ var SrcParseS = {
             }else if(dataObj.stype=="hipy_t4"){
                 play = JSON.parse(fetch(dataObj.surl+'&flag='+dataObj.flag+"&extend="+dataObj.sext+'&play='+vipUrl, {timeout: 10000}));
             }
+            log(play);
             if(play.url.startsWith("pics://")){
                 return play.url;
             }if(play.url.startsWith("select://")){
