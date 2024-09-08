c0d2a921b5bc975efd95d70274b82889dd3ef31a
src48597962 <48597962@qq.com>
2024-09-08 21:29:17 +0800

    更新 'SrcJyPublic.js'

--- SrcJyPublic.js
+++ SrcJyPublic.js
@@ -784,7 +784,7 @@ function getide(is) {
 function getHistory(i){
     let h = [];
     let ruleTitle = MY_RULE._title || MY_RULE.title;
-    i = i*3 || 0;
+    i = i ? i*3 : 0;
     let items = JSON.parse(fetch('hiker://history')).filter(v=> v.type!="网页浏览" && JSON.parse(v.params).title==ruleTitle).slice(i, 3);
     items.forEach(item=>{
         try{
