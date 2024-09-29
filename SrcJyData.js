a8941298b63dad2a6305c4679845cfad508da594
src48597962 <48597962@qq.com>
2024-09-29 16:44:30 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -800,6 +800,7 @@ function getSsData(name, jkdata, page) {
             if(api_type=="hipy_t4"){
                 json = JSON.parse(getHtml(jkdata.url+"&wd="+name+"&extend="+jkdata.ext+"&quick=false", headers));
             }else if(api_type=="hipy_t3"){
+                log(MY_RULE._title||MY_RULE.title);
                 GM.setSelfKey(MY_RULE._title||MY_RULE.title);
                 let drpy = GM.defineModule("SrcJyDrpy", config.依赖.match(/http(s)?:\/\/.*\//)[0] + "SrcJyDrpy.js").get(jkdata);
                 json = JSON.parse(drpy.search(name, 0, page));
