9a315d5633947e0fff8aad81e6ce1c5c9d73f4dd
src48597962 <48597962@qq.com>
2024-09-29 15:54:02 +0800

    更新 'SrcJyData.js'

--- SrcJyData.js
+++ SrcJyData.js
@@ -87,7 +87,9 @@ function getYiData(jkdata, batchTest) {
             vodhost = getHome(listurl);
         }
     } else if (api_type == 'hipy_t3') {
-        GM.clear("SrcJyDrpy");
+        //GM.clear("SrcJyDrpy");
+        let g = GM.defineModule("SrcJyDrpy", config.依赖.match(/http(s)?:\/\/.*\//)[0] + "SrcJyDrpy.js");
+        log($.type(g));
         var drpy = GM.defineModule("SrcJyDrpy", config.依赖.match(/http(s)?:\/\/.*\//)[0] + "SrcJyDrpy.js").get(jkdata);
         let rule = drpy.getRule();
         detailurl = rule.detailUrl || "";
