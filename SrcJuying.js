2473d283b77c289ae02d86f05f175000bbfd3413
src48597962 <48597962@qq.com>
2024-10-21 14:19:03 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -633,7 +633,7 @@ function dianboerji() {
     
     //底部说明
     d.push({
-        desc: '‘‘’’<small><small><font color=#bfbfbf>本规则仅供学习交流使用，任何团体或个人不得以任何形式传播其整体或部分内容。</font></small></small>',
+        desc: '‘‘’’<small><small><font color=#bfbfbf>本规则为空壳小程序，仅供学习交流使用，不提供任何内容。</font></small></small>',
         url: 'toast://温馨提示：且用且珍惜！',
         col_type: 'text_center_1',
         extra: {
