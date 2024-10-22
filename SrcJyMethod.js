3f054631232ee6fba20b594e7db2d4146dc80350
src48597962 <48597962@qq.com>
2024-10-22 10:25:02 +0800

    更新 'SrcJyMethod.js'

--- SrcJyMethod.js
+++ SrcJyMethod.js
@@ -1,7 +1,7 @@
 // 本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
 // 一些多处会调用但不用默认加载的方法
-// 注入js模似点击
-function jsClick(playUrl) {
+// extra.js，可用于注入js模似点击
+function extraJS(playUrl) {
     if(/jqqzx\.me/.test(playUrl)){//|dadazhu\.me
         return $.toString(() => {
             function check() {
