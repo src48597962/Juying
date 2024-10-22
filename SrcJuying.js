16027128f9c209c4f9d6427ade028414a4405976
src48597962 <48597962@qq.com>
2024-10-22 11:20:46 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -557,7 +557,6 @@ function dianboerji() {
                 dataObj.sext = jkdata.ext;
             }
             let novel = erdata.detailtype=="小说";
-            /*
             if(novel){
                 lazy = $("#readTheme##autoPage#").rule((dataObj)=>{
                     let vipUrl = MY_URL.split('##')[1].split('#')[0];
@@ -588,44 +587,7 @@ function dianboerji() {
                     });
                     setResult(d)
                 }, dataObj);
-            }else{
-                */
-                lazy = $("").lazyRule((dataObj) => {
-                    log(input);
-                    return input + $("#readTheme##autoPage#").rule((dataObj)=>{
-                        log(MY_URL);
-                        let vipUrl = MY_URL.split('##')[1].split('#')[0];
-                        let play;
-                        if(dataObj.stype=="hipy_t3"){
-                            let sdata = {name: dataObj.sname, url: dataObj.surl, ext: dataObj.sext}
-                            let drpy = GM.defineModule("SrcJyDrpy", config.依赖.replace(/[^/]*$/,'') + "SrcJyDrpy.js").get(sdata);
-                            play = JSON.parse(drpy.play(dataObj.flag, vipUrl, []));
-                        }else if(dataObj.stype=="hipy_t4"){
-                            play = JSON.parse(request(dataObj.surl+'&flag='+dataObj.sname+"&extend="+dataObj.sext+'&play='+vipUrl));
-                        }
-                        let data = JSON.parse(play.url.replace('novel://',''));
-                        let d = [];
-                        d.push({
-                            title: '<big>' + data.title + '</big>',
-                            col_type: 'rich_text',
-                            extra: {
-                                click: true
-                            }
-                        });
-                        d.push({
-                            title: "　　" + data.content.replace(/(&nbsp;){1,}/g, '　　').replace(/\n/g, "<p>　　"),
-                            col_type: "rich_text",
-                            extra: {
-                                textSize: 18,
-                                click: true
-                            }
-                        });
-                        setResult(d)
-                    }, dataObj);
-                    //require(config.依赖.replace(/[^/]*$/,'') + 'SrcParseS.js');
-                    //return SrcParseS.聚影(input, dataObj);
-                }, dataObj);
-            //}
+            }
             
             let playSet = storage0.getItem('playSet') || {};
             let len = 列表.slice(0, 10).concat(列表.slice(-10)).reduce((max, str) => Math.max(max, str.split('$')[0].replace(name,"").trim().length), 0);
@@ -638,14 +600,24 @@ function dianboerji() {
                 playtitle = playtitle==name?'正片':playtitle.replace(name+" - ","").replace(name+"_","").replace(name,"").replace("《》","").replace("<>","").replace("[]","").trim();
                 playtitle = decodeURIComponent(playtitle);
                 let playurl = (novel?"hiker://empty##":"")+列表[i].split('$')[1].trim();
+                let playid = name + "_选集_" + (pageid?pageid+"_":"") + i;
 
                 let obj = {
-                    id: name + "_选集_" + (pageid?pageid+"_":"") + i,
+                    id: playid,
                     playUrl: playurl,
                     sniffer: sniffer,
                     cachem3u8: playSet.cachem3u8
                 }
                 let extra = getPlayExtra(obj);
+
+                if(!novel){
+                    dataObj.id = playid;
+                    dataObj.sniffer = sniffer;
+                    lazy = $("").lazyRule((dataObj) => {
+                        require(config.依赖.replace(/[^/]*$/,'') + 'SrcParseS.js');
+                        return SrcParseS.聚影(input, dataObj);
+                    }, dataObj);
+                }
 
                 d.push({
                     title: "““””<small>"+playtitle.replace(/第|集|话|期|new|最新|新/g, '')+"</small>",
