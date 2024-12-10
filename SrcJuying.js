4fb211a0c567f5f7b86b592575e1a8002959f83d
src48597962 <48597962@qq.com>
2024-12-10 09:31:51 +0800

    更新 'SrcJuying.js'

--- SrcJuying.js
+++ SrcJuying.js
@@ -277,9 +277,7 @@ function dianboerji() {
         require(config.依赖.replace(/[^/]*$/,'') + 'SrcJyData.js');
         log('开始获取二级数据');
         let t1 = new Date().getTime();
-        log(jkdata);
         erdata = getErData(jkdata);
-        log(erdata);
         let t2 = new Date().getTime();
         log('获取二级数据完成，耗时：' + (t2-t1) + 'ms');
         if(erdata.lists.length>0){//只有二级选集列表获取到时才缓存
