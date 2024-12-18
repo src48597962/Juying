7283ee57c198553cb8ec171cffafac4dd7d81ed8
src48597962 <48597962@qq.com>
2024-12-18 09:25:10 +0800

    更新 'SrcJyPublic.js'

--- SrcJyPublic.js
+++ SrcJyPublic.js
@@ -120,7 +120,7 @@ function checkBoxUrl(input) {
             let isRunning = $.require('hiker://page/thirdstart?rule=nodejs').isRunning(nodejsID);
             if(!isRunning){
                 $.require('hiker://page/thirdstart?rule=nodejs').start(nodejsID);
-                tosat('正在启动notejs服务中');
+                toast('正在启动notejs服务中');
                 let c = 0;
                 while (c<20) {
                     java.lang.Thread.sleep(1000);
