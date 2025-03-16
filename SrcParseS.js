d8ec7c816de401d2b6df369ad5a680cb10c96f22
mybb520 <48597962@qq.com>
2025-03-16 14:42:20 +0000

    更新 SrcParseS.js

--- SrcParseS.js
+++ SrcParseS.js
@@ -141,7 +141,7 @@ var SrcParseS = {
                         s.init(cache);
                         let url = s.JPUrlDec(vipUrl) + "#isVideo=true#";
                         //s = loadJavaClass("hiker://files/cache/bidi.dex", "com.rule.jianpian");
-                        //s.finish();
+                        s.finish();
                         return url;
                     } catch (e) {
                         return "toast://荐片播放失败";
