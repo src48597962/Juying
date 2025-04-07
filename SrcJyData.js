23614ef481f0790363ef649737d25d2198b0d841
mybb520 <48597962@qq.com>
2025-04-07 09:26:54 +0000

    更新 SrcJyData.js

--- SrcJyData.js
+++ SrcJyData.js
@@ -1041,6 +1041,8 @@ function getSsData(name, jkdata, page) {
                             let 副标 = (extdata["搜索副标题"]||extdata["副标题"]);
                             let id = getJsonValue(sslist[i],'id');
                             let en = getJsonValue(sslist[i],副标);
+                            log(id);
+                            log(en);
 
                             let title = 标题.includes('&&')?getBetweenStr(JSON.stringify(sslist[i]), 标题):getJsonValue(sslist[i],标题);
                             let img = 图片.includes('&&')?getBetweenStr(JSON.stringify(sslist[i]), 图片):getJsonValue(sslist[i],图片);
