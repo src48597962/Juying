c697ad27b68ae9c330291b9efe7f860e1cd88abe
mybb520 <48597962@qq.com>
2025-04-08 13:15:02 +0000

    更新 SrcJyData.js

--- SrcJyData.js
+++ SrcJyData.js
@@ -1418,7 +1418,7 @@ function getErData(jkdata, erurl) {
                 }
                 log(tabs);
                 let conthtml = getBetweenStr(html, extdata["播放二次截取"], 1);
-                let contlist = getBetweenStrS(conthtml, extdata["播放数组"]||"id=\"playList&&</dd>||<ul&&</ul>");
+                let contlist = getBetweenStrS(conthtml, extdata["播放数组"]||"id=\"playList&&</dd>||<ul class=\"stui-content__playlist&&</ul>");
                 log(contlist);
                 for (let i = 0; i < contlist.length; i++) {
                     let bfline = extdata["播放列表"] ? getBetweenStrS(contlist[i], extdata["播放列表"]) : pdfa(contlist[i], "body&&a");
