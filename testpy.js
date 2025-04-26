767197481dd0ddd195352347d6e3a952b0205c97
mybb520 <48597962@qq.com>
2025-04-26 09:15:08 +0000

    更新 testpy.js

--- testpy.js
+++ testpy.js
@@ -27,12 +27,13 @@ d.push({
 setResult(d);
 //其他小程序调用请用"hiker://files/plugins/chaquopy/PythonHiker.js"
 const PythonHiker = $.require("hiker://files/plugins/chaquopy/PythonHiker.js");
-const pyfile = "https://ghproxy.net/https://raw.githubusercontent.com/JJBJJ/PyramidStore/refs/heads/main/plugin/app/%E5%A5%87%E8%BF%B9APP.py";
+const pyfile = "https://ghproxy.net/https://raw.githubusercontent.com/JJBJJ/PyramidStore/refs/heads/main/plugin/html/%E9%AA%9A%E7%81%AB%E7%94%B5%E5%BD%B1.py";
+//const pyfile = "/storage/emulated/0/Android/data/com.example.hikerview/files/Documents/plugins/chaquopy/libs_py/胖虎.py";
 let testModule = PythonHiker.runPy(pyfile).callAttr("Spider");
-//let sp = testModule.Spider();
-//PythonHiker.callFunc(testModule, "init", []);
-//PythonHiker.callFunc(testModule, "init", [])
-let formatJo = PythonHiker.callFunc(testModule, "homeContent", False);
+PythonHiker.callFunc(testModule, "init", []);
+//let formatJo = PythonHiker.callFunc(testModule, "homeContent", false); //筛选分类主页
+let formatJo = PythonHiker.callFunc(testModule, "detailContent", ['/movie/49398.html']);
+
 log(formatJo);
 //从指定路径加载py模块并返回模块句柄
 /*let testModule = PythonHiker.runPy(getPath("hiker://files/plugins/chaquopy/libs_py/test.py").slice(7), "__main__");
