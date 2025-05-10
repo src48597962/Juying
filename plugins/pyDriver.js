b205f9d777f5226172ad350c7d639a234275db7c
mybb520 <48597962@qq.com>
2025-05-10 12:49:17 +0000

    更新 plugins/pyDriver.js

--- plugins/pyDriver.js
+++ plugins/pyDriver.js
@@ -12,9 +12,8 @@ function getPyFile(url) {
 function initPyModule(api_url) {
     log("aaa");
     log(Object.keys(PythonHiker.runPy(getPyFile(api_url))));
-    log($.type(PythonHiker.runPy(getPyFile(api_url)).get("getName")));
+    log($.type(PythonHiker.runPy(getPyFile(api_url)).get()));
     log($.type(PythonHiker.runPy(getPyFile(api_url)).get("detailContent")));
-    log(PythonHiker.runPy(getPyFile(api_url)).asList());
     log($.type(PythonHiker.runPy(getPyFile(api_url)).getClass().getName()));
     var pyModule = PythonHiker.runPy(getPyFile(api_url)).callAttr("Spider");
     log("bbb");
