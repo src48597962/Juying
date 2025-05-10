a5c49ae6e53604180be8dce714eb9abb05e68fcb
mybb520 <48597962@qq.com>
2025-05-10 11:51:22 +0000

    更新 plugins/pyDriver.js

--- plugins/pyDriver.js
+++ plugins/pyDriver.js
@@ -1,19 +1,19 @@
 var PythonHiker = $.require("hiker://files/plugins/chaquopy/PythonHiker.js");
-log("aaa");
+// py源获取本地文件路径
+function getPyFile(url) {
+    if(url.startsWith('hiker')){
+        url = getPath(url).slice(7);
+    }else if(url.startsWith('file://')){
+        url = url.slice(7);
+    }
+    return url;
+}
+// 初始化py源修正相关模块方法
 function initPyModule(api_url) {
+    log("aaa");
     var pyModule = PythonHiker.runPy(getPyFile(api_url)).callAttr("Spider");
     log("bbb");
-    // py源获取本地文件路径
-    function getPyFile(url) {
-        if(url.startsWith('hiker')){
-            url = getPath(url).slice(7);
-        }else if(url.startsWith('file://')){
-            url = url.slice(7);
-        }
-        return url;
-    }
-    log("进来了");
-    // 初始化py源修正相关模块方法
+    
     if(!pyModule.get("getName")){
         log("add getName");
         pyModule.put("getName", PythonHiker.wrapperJsFunc(function(){
