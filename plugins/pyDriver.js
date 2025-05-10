26104afcf164617c6001a3972d58e538833e0511
mybb520 <48597962@qq.com>
2025-05-10 12:27:37 +0000

    更新 plugins/pyDriver.js

--- plugins/pyDriver.js
+++ plugins/pyDriver.js
@@ -11,8 +11,7 @@ function getPyFile(url) {
 // 初始化py源修正相关模块方法
 function initPyModule(api_url) {
     log("aaa");
-    log($.type(PythonHiker.Builtins.get("getName")));
-    log($.type(PythonHiker.Builtins.get("Spider")));
+    log($.type(PythonHiker.runPy(getPyFile(api_url))));
     var pyModule = PythonHiker.runPy(getPyFile(api_url)).callAttr("Spider");
     log("bbb");
     
