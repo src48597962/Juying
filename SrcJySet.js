88c57f3b9382a8682a9392682a18707784f4e11f
src48597962 <48597962@qq.com>
2025-07-11 04:59:45 +0000

    更新 SrcJySet.js

--- SrcJySet.js
+++ SrcJySet.js
@@ -1,5 +1,5 @@
 ////本代码仅用于个人学习，请勿用于其他作用，下载后请24小时内删除，代码虽然是公开学习的，但请尊重作者，应留下说明
-require((config.聚影||config.依赖).replace(/[^/]*$/,'') + 'SrcJyPublic.js');//加载公共文件
+require((config.聚影||getPublicItem('聚影','')).replace(/[^/]*$/,'') + 'SrcJyPublic.js');//加载公共文件
 
 function SRCSet() {
     addListener("onClose", $.toString(() => {
